9ae4fafc05867db1a260b131c2c4520abbb2ad2f
Jonny <jonny@smartcontract.com>
2021-04-23 13:54:47 +0100

    Deliberate fail

--- client/blockchain_test.go
+++ client/blockchain_test.go
@@ -28,7 +28,7 @@ var _ = Describe("Client", func() {
 		Expect(privateKey).To(Equal(wallets.Default().PrivateKey()))
 		Expect(address).To(Equal(wallets.Default().Address()))
 	},
-		Entry("ethereum hardhat", NewEthereumHardhat, "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
+		Entry("ethereum hardhat", NewEthereumHardhat, "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "WRONG0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
 	)
 
 	DescribeTable("deploy the storage contract", func(initFunc BlockchainNetworkInit) {
