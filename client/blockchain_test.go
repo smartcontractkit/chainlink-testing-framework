bc1452baa733a631138007a0bda70f3c455b7236
Adam Hamrick <adam.hamrick@smartcontract.com>
2021-04-26 18:12:34 -0400

    Some cleanup and documentation

--- client/blockchain_test.go
+++ client/blockchain_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"integrations-framework/config"
+	"math/big"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
@@ -28,7 +29,7 @@ var _ = Describe("Client", func() {
 		Expect(privateKey).To(Equal(wallets.Default().PrivateKey()))
 		Expect(address).To(Equal(wallets.Default().Address()))
 	},
-		Entry("ethereum hardhat", NewEthereumHardhat, "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "WRONG0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
+		Entry("Ethereum Hardhat", NewEthereumHardhat, "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "WRONG0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
 	)
 
 	DescribeTable("deploy the storage contract", func(initFunc BlockchainNetworkInit) {
@@ -40,7 +41,45 @@ var _ = Describe("Client", func() {
 		err = client.DeployStorageContract(wallets.Default())
 		Expect(err).ShouldNot(HaveOccurred())
 	},
-		Entry("ethereum hardhat", NewEthereumHardhat),
+		Entry("Ethereum Hardhat", NewEthereumHardhat),
+	)
+
+	DescribeTable("create new wallet configurations", func(
+		initFunc BlockchainNetworkInit,
+		privateKey string,
+		address string,
+	) {
+		networkConfig := initFunc(conf)
+		wallets, err := networkConfig.Wallets()
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(privateKey).To(Equal(wallets.Default().PrivateKey()))
+		Expect(address).To(Equal(wallets.Default().Address()))
+	},
+		Entry("Ethereum Hardhat", NewEthereumHardhat,
+			"ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+			"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
+	)
+
+	DescribeTable("send basic ETH transactions", func(
+		initFunc BlockchainNetworkInit,
+	) {
+		networkConfig := initFunc(conf)
+		wallets, err := networkConfig.Wallets()
+		Expect(err).ShouldNot(HaveOccurred())
+		client, err := NewBlockchainClient(networkConfig)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		toWallet, err := wallets.Wallet(1)
+		Expect(err).ShouldNot(HaveOccurred())
+		txHash, err := client.SendNativeTransaction(wallets.Default(), toWallet.Address(), big.NewInt(500))
+		Expect(err).ShouldNot(HaveOccurred())
+
+		// Get latest block and make sure our transaction went through
+		// This can be tricky, there's lots to potentially parameterize around this
+		// Current idea I'm working with is to include a txTimeout config variable
+		// There's more potential issues I thought of but forgot
+	},
+		Entry("Ethereum Hardhat", NewEthereumHardhat),
 	)
 })
 
