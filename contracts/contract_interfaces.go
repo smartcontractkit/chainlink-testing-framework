f9b76e86c932555ef2d16db07d5eeef827216c03
Adam Hamrick <adam.hamrick@smartcontract.com>
2021-05-10 17:29:21 -0400

    Absolute basic interactions

--- contracts/contract_interfaces.go
+++ contracts/contract_interfaces.go
@@ -3,13 +3,9 @@ package contracts
 import (
 	"context"
 	"math/big"
-)
 
-// Storage represents generalized interactions with the stoarge contract
-type Storage interface {
-	Get(context.Context) (*big.Int, error)
-	Set(context.Context, *big.Int) error
-}
+	"github.com/ethereum/go-ethereum/common"
+)
 
 type FluxAggregator interface {
 	Description(context.Context) (string, error)
@@ -17,6 +13,15 @@ type FluxAggregator interface {
 
 type LinkToken interface {
 	Name(context.Context) (string, error)
+}
+
+type OffchainAggregator interface {
+	Link(ctxt context.Context) (common.Address, error)
+}
+
+type Storage interface {
+	Get(context.Context) (*big.Int, error)
+	Set(context.Context, *big.Int) error
 }
 
 type VRF interface {
--- contracts/ethereum_contracts.go
+++ contracts/ethereum_contracts.go
@@ -12,65 +12,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// EthereumStorage acts as a conduit for the ethereum version of the storage contract
-type EthereumStorage struct {
-	client       *client.EthereumClient
-	store        *ethereum.Store
-	callerWallet client.BlockchainWallet
-}
-
-// NewEthereumStorage creates a new instance of the storage contract for ethereum chains
-func NewEthereumStorage(
-	client *client.EthereumClient,
-	store *ethereum.Store,
-	callerWallet client.BlockchainWallet,
-) Storage {
-
-	return &EthereumStorage{
-		client:       client,
-		store:        store,
-		callerWallet: callerWallet,
-	}
-}
-
-// DeployStorageContract deploys a vanilla storage contract that is a value store
-func DeployStorageContract(ethClient *client.EthereumClient, fromWallet client.BlockchainWallet) (Storage, error) {
-	_, _, instance, err := ethClient.DeployContract(fromWallet, func(
-		auth *bind.TransactOpts,
-		backend bind.ContractBackend,
-	) (common.Address, *types.Transaction, interface{}, error) {
-		return ethereum.DeployStore(auth, backend)
-	})
-	if err != nil {
-		return nil, err
-	}
-	return NewEthereumStorage(ethClient, instance.(*ethereum.Store), fromWallet), nil
-}
-
-// Set sets a value in the storage contract
-func (e *EthereumStorage) Set(ctxt context.Context, value *big.Int) error {
-	opts, err := e.client.TransactionOpts(e.callerWallet, common.Address{}, big.NewInt(0), common.Hash{})
-	if err != nil {
-		return err
-	}
-
-	transaction, err := e.store.Set(opts, value)
-	if err != nil {
-		return err
-	}
-	return e.client.WaitForTransaction(transaction.Hash())
-}
-
-// Get retrieves a set value from the storage contract
-func (e *EthereumStorage) Get(ctxt context.Context) (*big.Int, error) {
-	opts := &bind.CallOpts{
-		From:    common.HexToAddress(e.callerWallet.Address()),
-		Pending: true,
-		Context: ctxt,
-	}
-	return e.store.Get(opts)
-}
-
 // EthereumFluxAggregator represents the basic flux aggregation contract
 type EthereumFluxAggregator struct {
 	client         *client.EthereumClient
@@ -174,6 +115,94 @@ func (l *EthereumLinkToken) Name(ctxt context.Context) (string, error) {
 		Context: ctxt,
 	}
 	return l.linkToken.Name(opts)
+}
+
+type EthereumOffchainAggregator struct {
+	client       *client.EthereumClient
+	ocr          *ethereum.OffchainAggregator
+	callerWallet client.BlockchainWallet
+}
+
+func NewEthereumOffchainAggregator(
+	client *client.EthereumClient,
+	ocr *ethereum.OffchainAggregator,
+	callerWallet client.BlockchainWallet,
+) OffchainAggregator {
+
+	return &EthereumOffchainAggregator{
+		client:       client,
+		ocr:          ocr,
+		callerWallet: callerWallet,
+	}
+}
+
+// Link returns the LINK contract address on the EVM chain
+func (o *EthereumOffchainAggregator) Link(ctxt context.Context) (common.Address, error) {
+	opts := &bind.CallOpts{
+		From:    common.HexToAddress(o.callerWallet.Address()),
+		Pending: true,
+		Context: ctxt,
+	}
+	return o.ocr.LINK(opts)
+}
+
+// EthereumStorage acts as a conduit for the ethereum version of the storage contract
+type EthereumStorage struct {
+	client       *client.EthereumClient
+	store        *ethereum.Store
+	callerWallet client.BlockchainWallet
+}
+
+// NewEthereumStorage creates a new instance of the storage contract for ethereum chains
+func NewEthereumStorage(
+	client *client.EthereumClient,
+	store *ethereum.Store,
+	callerWallet client.BlockchainWallet,
+) Storage {
+
+	return &EthereumStorage{
+		client:       client,
+		store:        store,
+		callerWallet: callerWallet,
+	}
+}
+
+// DeployStorageContract deploys a vanilla storage contract that is a value store
+func DeployStorageContract(ethClient *client.EthereumClient, fromWallet client.BlockchainWallet) (Storage, error) {
+	_, _, instance, err := ethClient.DeployContract(fromWallet, func(
+		auth *bind.TransactOpts,
+		backend bind.ContractBackend,
+	) (common.Address, *types.Transaction, interface{}, error) {
+		return ethereum.DeployStore(auth, backend)
+	})
+	if err != nil {
+		return nil, err
+	}
+	return NewEthereumStorage(ethClient, instance.(*ethereum.Store), fromWallet), nil
+}
+
+// Set sets a value in the storage contract
+func (e *EthereumStorage) Set(ctxt context.Context, value *big.Int) error {
+	opts, err := e.client.TransactionOpts(e.callerWallet, common.Address{}, big.NewInt(0), common.Hash{})
+	if err != nil {
+		return err
+	}
+
+	transaction, err := e.store.Set(opts, value)
+	if err != nil {
+		return err
+	}
+	return e.client.WaitForTransaction(transaction.Hash())
+}
+
+// Get retrieves a set value from the storage contract
+func (e *EthereumStorage) Get(ctxt context.Context) (*big.Int, error) {
+	opts := &bind.CallOpts{
+		From:    common.HexToAddress(e.callerWallet.Address()),
+		Pending: true,
+		Context: ctxt,
+	}
+	return e.store.Get(opts)
 }
 
 // EthereumVRF represents a VRF contract
