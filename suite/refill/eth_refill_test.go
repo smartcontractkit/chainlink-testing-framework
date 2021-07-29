6017c9414eb937a9672e8c14293b0d17b9520e7c
Jonny <jonny@smartcontract.com>
2021-07-29 13:34:51 +0100

    Rework the ETH refill test to better utilise Ginkgo

--- suite/refill/eth_refill_test.go
+++ suite/refill/eth_refill_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/ethereum/go-ethereum/common"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"github.com/rs/zerolog/log"
 	"github.com/smartcontractkit/integrations-framework/actions"
@@ -16,89 +15,113 @@ import (
 	"time"
 )
 
-var _ = Describe("ETH refill suite", func() {
+var _ = Describe("FluxAggregator ETH Refill", func() {
 	var s *actions.DefaultSuiteSetup
+	var adapter environment.ExternalAdapter
+	var nodes []client.Chainlink
+	var nodeAddresses []common.Address
 	var err error
+	var fluxInstance contracts.FluxAggregator
 
-	DescribeTable("Can work after refill", func(
-		envInitFunc environment.K8sEnvSpecInit,
-		networkInitFunc client.BlockchainNetworkInit,
-		fluxOptions contracts.FluxAggregatorOptions,
-	) {
-		s, err = actions.DefaultLocalSetup(envInitFunc, networkInitFunc)
-		Expect(err).ShouldNot(HaveOccurred())
-		fluxInstance, err := s.Deployer.DeployFluxAggregatorContract(s.Wallets.Default(), fluxOptions)
-		Expect(err).ShouldNot(HaveOccurred())
-		err = fluxInstance.Fund(s.Wallets.Default(), big.NewInt(0), big.NewInt(1e18))
-		Expect(err).ShouldNot(HaveOccurred())
-		err = fluxInstance.UpdateAvailableFunds(context.Background(), s.Wallets.Default())
-		Expect(err).ShouldNot(HaveOccurred())
-		clNodes, err := environment.GetChainlinkClients(s.Env)
-		Expect(err).ShouldNot(HaveOccurred())
-		nodeAddrs, err := actions.ChainlinkNodeAddresses(clNodes)
-		Expect(err).ShouldNot(HaveOccurred())
-
-		err = fluxInstance.SetOracles(s.Wallets.Default(),
-			contracts.SetOraclesOptions{
-				AddList:            nodeAddrs,
-				RemoveList:         []common.Address{},
-				AdminList:          nodeAddrs,
-				MinSubmissions:     3,
-				MaxSubmissions:     3,
-				RestartDelayRounds: 0,
-			})
-		Expect(err).ShouldNot(HaveOccurred())
-		oracles, err := fluxInstance.GetOracles(context.Background())
-		Expect(err).ShouldNot(HaveOccurred())
-		log.Info().Str("Oracles", strings.Join(oracles, ",")).Msg("oracles set")
-
-		adapter, err := environment.GetExternalAdapter(s.Env)
-		Expect(err).ShouldNot(HaveOccurred())
-
-		os := &client.PipelineSpec{
-			URL:         adapter.ClusterURL() + "/five",
-			Method:      "POST",
-			RequestData: "{}",
-			DataPath:    "data,result",
-		}
-		ost, err := os.String()
-		Expect(err).ShouldNot(HaveOccurred())
-		for _, n := range clNodes {
-			fluxSpec := &client.FluxMonitorJobSpec{
-				Name:              "flux_monitor",
-				ContractAddress:   fluxInstance.Address(),
-				PollTimerPeriod:   15 * time.Second, // min 15s
-				PollTimerDisabled: false,
-				ObservationSource: ost,
-			}
-			_, err = n.CreateJob(fluxSpec)
+	BeforeSuite(func() {
+		By("Deploying the environment", func() {
+			s, err = actions.DefaultLocalSetup(
+				environment.NewChainlinkCluster("../../", 3),
+				client.NewHardhatNetwork,
+			)
 			Expect(err).ShouldNot(HaveOccurred())
-		}
-		// fund enough for one round
-		err = actions.FundChainlinkNodes(clNodes, s.Client, s.Wallets.Default(), big.NewInt(1e16), nil)
-		Expect(err).ShouldNot(HaveOccurred())
-		err = adapter.SetVariable(6)
-		Expect(err).ShouldNot(HaveOccurred())
-		err = fluxInstance.AwaitNextRoundFinalized(context.Background())
-		Expect(err).ShouldNot(HaveOccurred())
-		// no ETH
-		err = adapter.SetVariable(5)
-		Expect(err).ShouldNot(HaveOccurred())
-		err = fluxInstance.AwaitNextRoundFinalized(context.Background())
-		Expect(err).Should(HaveOccurred())
-		// refill and check if it works
-		err = actions.FundChainlinkNodes(clNodes, s.Client, s.Wallets.Default(), big.NewInt(2e18), nil)
-		Expect(err).ShouldNot(HaveOccurred())
-		err = fluxInstance.AwaitNextRoundFinalized(context.Background())
-		Expect(err).ShouldNot(HaveOccurred())
-		data, err := fluxInstance.GetContractData(context.Background())
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(data.LatestRoundData.Answer.Int64()).Should(Equal(int64(5)))
-	},
-		Entry("on Ethereum Hardhat", client.NewHardhatNetwork, contracts.DefaultFluxAggregatorOptions()),
-	)
+			adapter, err = environment.GetExternalAdapter(s.Env)
+			Expect(err).ShouldNot(HaveOccurred())
+			nodes, err = environment.GetChainlinkClients(s.Env)
+			Expect(err).ShouldNot(HaveOccurred())
+			nodeAddresses, err = actions.ChainlinkNodeAddresses(nodes)
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+	})
 
-	AfterEach(func() {
-		s.Env.TearDown()
+	BeforeEach(func() {
+		By("Deploying and fund the contract", func() {
+			fluxInstance, err = s.Deployer.DeployFluxAggregatorContract(
+				s.Wallets.Default(),
+				contracts.DefaultFluxAggregatorOptions(),
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+			err = fluxInstance.Fund(s.Wallets.Default(), big.NewInt(0), big.NewInt(1e18))
+			Expect(err).ShouldNot(HaveOccurred())
+			err = fluxInstance.UpdateAvailableFunds(context.Background(), s.Wallets.Default())
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		By("Setting FluxAggregator options", func() {
+			err = fluxInstance.SetOracles(s.Wallets.Default(),
+				contracts.SetOraclesOptions{
+					AddList:            nodeAddresses,
+					RemoveList:         []common.Address{},
+					AdminList:          nodeAddresses,
+					MinSubmissions:     3,
+					MaxSubmissions:     3,
+					RestartDelayRounds: 0,
+				})
+			Expect(err).ShouldNot(HaveOccurred())
+			oracles, err := fluxInstance.GetOracles(context.Background())
+			Expect(err).ShouldNot(HaveOccurred())
+			log.Info().Str("Oracles", strings.Join(oracles, ",")).Msg("oracles set")
+		})
+
+		By("Adding FluxAggregator jobs to nodes", func() {
+			os := &client.PipelineSpec{
+				URL:         adapter.ClusterURL() + "/five",
+				Method:      "POST",
+				RequestData: "{}",
+				DataPath:    "data,result",
+			}
+			ost, err := os.String()
+			Expect(err).ShouldNot(HaveOccurred())
+			for _, n := range nodes {
+				fluxSpec := &client.FluxMonitorJobSpec{
+					Name:              "flux_monitor",
+					ContractAddress:   fluxInstance.Address(),
+					PollTimerPeriod:   15 * time.Second, // min 15s
+					PollTimerDisabled: false,
+					ObservationSource: ost,
+				}
+				_, err = n.CreateJob(fluxSpec)
+				Expect(err).ShouldNot(HaveOccurred())
+			}
+		})
+
+		By("Funding ETH for a single round", func() {
+			err = actions.FundChainlinkNodes(nodes, s.Client, s.Wallets.Default(), big.NewInt(1e16), nil)
+			Expect(err).ShouldNot(HaveOccurred())
+			err = adapter.SetVariable(6)
+			Expect(err).ShouldNot(HaveOccurred())
+			err = fluxInstance.AwaitNextRoundFinalized(context.Background())
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		By("Draining ETH on the nodes", func() {
+			err = adapter.SetVariable(5)
+			Expect(err).ShouldNot(HaveOccurred())
+			err = fluxInstance.AwaitNextRoundFinalized(context.Background())
+			Expect(err).Should(HaveOccurred())
+		})
+	})
+
+	Describe("with FluxAggregator", func() {
+		It("should refill and await the next round", func() {
+			err = actions.FundChainlinkNodes(nodes, s.Client, s.Wallets.Default(), big.NewInt(2e18), nil)
+			Expect(err).ShouldNot(HaveOccurred())
+			err = fluxInstance.AwaitNextRoundFinalized(context.Background())
+			Expect(err).ShouldNot(HaveOccurred())
+			data, err := fluxInstance.GetContractData(context.Background())
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(data.LatestRoundData.Answer.Int64()).Should(Equal(int64(5)))
+		})
+	})
+
+	AfterSuite(func() {
+		By("Tearing down the environment", func() {
+			s.Env.TearDown()
+		})
 	})
 })
