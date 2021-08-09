e707792a52c6703beb5dd08952260d62a31ca4ab
Jonny <jonny@smartcontract.com>
2021-08-09 12:01:30 +0100

    Use shared private keys, ability to define block gas limit in config

--- config/config.go
+++ config/config.go
@@ -49,6 +49,7 @@ type NetworkConfig struct {
 	LinkTokenAddress     string        `mapstructure:"link_token_address" yaml:"link_token_address"`
 	MinimumConfirmations int           `mapstructure:"minimum_confirmations" yaml:"minimum_confirmations"`
 	GasEstimationBuffer  uint64        `mapstructure:"gas_estimation_buffer" yaml:"gas_estimation_buffer"`
+	BlockGasLimit        uint64        `mapstructure:"block_gas_limit" yaml:"block_gas_limit"`
 	PrivateKeyStore      PrivateKeyStore
 }
 
