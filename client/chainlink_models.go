ec680cab8bdebbc52b402adbebde738267aabb2f
aalu1418 <aalu1418@gmail.com>
2021-12-03 12:16:34 -0500

    ocr2 job spec

--- client/chainlink_models.go
+++ client/chainlink_models.go
@@ -582,6 +582,67 @@ observationSource                      = """
 	return marshallTemplate(specWrap, "OCR Job", ocrTemplateString)
 }
 
+// OCR2TaskJobSpec represents an OCR2 job that is given to other nodes, meant to communicate with the bootstrap node,
+// and provide their answers
+type OCR2TaskJobSpec struct {
+	Name                     string        `toml:"name"`
+	ContractID               string        `toml:"contractID"`                             // Address of the OCR contract/account(s)
+	Relay                    string        `toml:"relay"`                                  // Name of blockchain relay to use
+	RelayConfig              string        `toml:"relayConfig"`                            // Relay spec object in stringified form
+	P2PPeerID                string        `toml:"p2pPeerID"`                              // This node's P2P ID
+	P2PBootstrapPeers        []P2PData     `toml:"p2pBootstrapPeers"`                      // P2P ID of the bootstrap node
+	IsBootstrapPeer          bool          `toml:"isBootstrapPeer"`                        // Typically false
+	OCRKeyBundleID           string        `toml:"ocrKeyBundleID"`                         // ID of this node's OCR key bundle
+	MonitoringEndpoint       string        `toml:"monitoringEndpoint"`                     // Typically "chain.link:4321"
+	TransmitterID            string        `toml:"transmitterID"`                          // ID of address this node will use to transmit
+	BlockChainTimeout        time.Duration `toml:"blockchainTimeout"`                      // Optional
+	TrackerSubscribeInterval time.Duration `toml:"contractConfigTrackerSubscribeInterval"` // Optional
+	TrackerPollInterval      time.Duration `toml:"contractConfigTrackerPollInterval"`      // Optional
+	ContractConfirmations    int           `toml:"contractConfigConfirmations"`            // Optional
+	ObservationSource        string        `toml:"observationSource"`                      // List of commands for the chainlink node
+	JuelsPerFeeCoinSource    string        `toml:"juelsPerFeeCoinSource"`                  // List of commands to fetch JuelsPerFeeCoin value (used to calculate ocr payments)
+}
+
+// Type returns the type of the job
+func (o *OCR2TaskJobSpec) Type() string { return "offchainreporting2" }
+
+// String representation of the job
+func (o *OCR2TaskJobSpec) String() (string, error) {
+	// Results in /dns4//tcp/6690/p2p/12D3KooWAuC9xXBnadsYJpqzZZoB4rMRWqRGpxCrr2mjS7zCoAdN\
+	ocr2TemplateString := `type = "offchainreporting2"
+schemaVersion                          = 1
+blockchainTimeout                      ={{if not .BlockChainTimeout}} "20s" {{else}} {{.BlockChainTimeout}} {{end}}
+contractConfigConfirmations            ={{if not .ContractConfirmations}} 3 {{else}} {{.ContractConfirmations}} {{end}}
+contractConfigTrackerPollInterval      ={{if not .TrackerPollInterval}} "1m" {{else}} {{.TrackerPollInterval}} {{end}}
+contractConfigTrackerSubscribeInterval ={{if not .TrackerSubscribeInterval}} "2m" {{else}} {{.TrackerSubscribeInterval}} {{end}}
+name 																	 = "{{.Name}}"
+relay																	 = "{{.Relay}}"
+relayConfig														 = "{{.RelayConfig}}"
+contractID		                         = "{{.ContractID}}"
+{{if .P2PBootstrapPeers}}
+p2pBootstrapPeers                      = [
+  {{range $peer := .P2PBootstrapPeers}}
+  "{{$peer.PeerID}}@{{$peer.RemoteIP}}",
+  {{end}}
+]
+{{else}}
+p2pBootstrapPeers                      = []
+{{end}}
+isBootstrapPeer                        = {{.IsBootstrapPeer}}
+p2pPeerID                              = "{{.P2PPeerID}}"
+keyBundleID                            = "{{.OCRKeyBundleID}}"
+monitoringEndpoint                     ={{if not .MonitoringEndpoint}} "chain.link:4321" {{else}} "{{.MonitoringEndpoint}}" {{end}}
+transmitterAddress                     = "{{.TransmitterID}}"
+observationSource                      = """
+{{.ObservationSource}}
+"""
+juelsPerFeeCoinSource                  = """
+{{.JuelsPerFeeCoinSource}}
+"""`
+
+	return marshallTemplate(o, "OCR2 Job", ocr2TemplateString)
+}
+
 // VRFJobSpec represents a VRF job
 type VRFJobSpec struct {
 	Name               string `toml:"name"`
