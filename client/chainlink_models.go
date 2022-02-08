7985cec7241d3e88699f8713ff979c1d0c0ec459
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2022-02-08 11:15:28 +0100

    Remove default .MonitoringEndpoint output for OCR2 spec

--- client/chainlink_models.go
+++ client/chainlink_models.go
@@ -697,7 +697,7 @@ p2pBootstrapPeers                      = []
 isBootstrapPeer                        = {{.IsBootstrapPeer}}
 p2pPeerID                              = "{{.P2PPeerID}}"
 keyBundleID                            = "{{.KeyBundleID}}"
-monitoringEndpoint                     ={{if not .MonitoringEndpoint}} "chain.link:4321" {{else}} "{{.MonitoringEndpoint}}" {{end}}
+{{ if .MonitoringEndpoint }} monitoringEndpoint                     = "{{ .MonitoringEndpoint }}" {{ end }}
 transmitterAddress                     = "{{.TransmitterAddress}}"
 observationSource                      = """
 {{.ObservationSource}}
@@ -753,8 +753,8 @@ p2pBootstrapPeers                      = []
 isBootstrapPeer                        = {{.IsBootstrapPeer}}
 p2pPeerID                              = "{{.P2PPeerID}}"
 ocrKeyBundleID                         = "{{.OCRKeyBundleID}}"
-monitoringEndpoint                     ={{if not .MonitoringEndpoint}} "chain.link:4321" {{else}} "{{.MonitoringEndpoint}}" {{end}}
-transmitterID                     		 = "{{.TransmitterID}}"
+{{ if .MonitoringEndpoint }} monitoringEndpoint                     = "{{ .MonitoringEndpoint }}" {{ end }}
+transmitterID                          = "{{.TransmitterID}}"
 {{if .IsBootstrapPeer}}
 {{else}}
 observationSource                      = """
