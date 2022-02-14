d2a1cd8952bfe0408ca3608b657becfe042e3881
Tate <tate.exon@smartcontract.com>
2022-02-14 10:34:18 -0700

    Handle exit codes now that gauntlet terra is going to output something other than 0 all the time

--- gauntlet/gauntlet_test.go
+++ gauntlet/gauntlet_test.go
@@ -22,7 +22,7 @@ var _ = Describe("Gauntlet @unit", func() {
 		g, err := gauntlet.NewGauntlet()
 		Expect(err).ShouldNot(HaveOccurred(), "Could not get a new gauntlet struct")
 		out, err := g.ExecCommand([]string{}, []string{})
-		Expect(err).ShouldNot(HaveOccurred(), "Failed to execute a command")
+		Expect(err).Should(HaveOccurred(), "Failed to execute a command")
 		Expect(out).To(ContainSubstring("yarn"), "Did not contain expected output")
 	})
 	It("should find an expected error in the output", func() {
