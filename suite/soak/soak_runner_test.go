21c9a1fe399afae7b99ce9cee38481033244c29d
Tate <tate.exon@smartcontract.com>
2022-05-19 09:28:05 -0600

    oops, linting

--- suite/soak/soak_runner_test.go
+++ suite/soak/soak_runner_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 func TestOCRSoak(t *testing.T) {
-	RunSoakTest(t, "@soak-ocr", "chainlink-soak-ocr", 6, nil)
+	runSoakTest(t, "@soak-ocr", "chainlink-soak-ocr", 6, nil)
 }
 
 func TestKeeperSoak(t *testing.T) {
-	RunSoakTest(t, "@soak-keeper-block-time", "chainlink-soak-keeper", 6, nil)
+	runSoakTest(t, "@soak-keeper-block-time", "chainlink-soak-keeper", 6, nil)
 }
