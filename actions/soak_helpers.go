a0c047bf6861d8689774367bf63dfa60309b7da3
Adam Hamrick <adam.hamrick@smartcontract.com>
2022-06-09 17:24:54 -0400

    Use docker to compile

--- actions/soak_helpers.go
+++ actions/soak_helpers.go
@@ -16,31 +16,50 @@ import (
 
 // BuildGoTests builds the go tests to run, and returns a path to it, along with remote config options
 func BuildGoTests(executablePath, testsPath string) (string, error) {
-	exePath := filepath.Join(executablePath, "remote.test")
-	compileCmd := exec.Command("go", "test", "-ldflags=-s -w", "-c", testsPath, "-o", exePath) // #nosec G204
-	compileCmd.Env = os.Environ()
-	compileCmd.Env = append(compileCmd.Env, "CGO_ENABLED=0", "GOOS=linux", "GOARCH=amd64")
+	LoadConfigs()
+	dockerfilePath := filepath.Join(utils.SoakRoot, "Dockerfile.compiler")
+	testTargetDir := filepath.Join(utils.ProjectRoot, "generated_test_dir")
+	finalTestDestination := filepath.Join(utils.ProjectRoot, "remote.test")
+	// Clean up old test files if they're around
+	if _, err := os.Stat(finalTestDestination); err == nil {
+		if err = os.Remove(finalTestDestination); err != nil {
+			return "", nil
+		}
+	}
 
-	log.Info().Str("Test Directory", testsPath).Msg("Compiling tests")
-	compileOut, err := compileCmd.CombinedOutput()
+	// TODO: Docker has a Go API, but it was oddly complicated and not at all documented, and kept failing.
+	// So for now, we're doing the tried and true method of plain commands.
+	dockerBuildCmd := exec.Command("docker", "build", "-t", "test-compiler", "-f",
+		dockerfilePath, "--output", testTargetDir, utils.ProjectRoot) // #nosec G204
+	dockerBuildCmd.Env = os.Environ()
+	log.Info().Str("Docker File", dockerfilePath).Msg("Compiling tests")
+	compileOut, err := dockerBuildCmd.CombinedOutput()
 	log.Debug().
 		Str("Output", string(compileOut)).
-		Str("Command", compileCmd.String()).
+		Str("Command", dockerBuildCmd.String()).
 		Msg("Ran command")
 	if err != nil {
-		return "", fmt.Errorf("Env: %s\nCommand: %s\nCommand Output: %s, %w", compileCmd.Env, compileCmd.String(), string(compileOut), err)
+		return "", err
 	}
 
-	_, err = os.Stat(exePath)
+	err = os.Rename(filepath.Join(testTargetDir, "remote.test"), finalTestDestination)
 	if err != nil {
-		return "", fmt.Errorf("Expected '%s' to exist, %w", exePath, err)
+		return "", err
 	}
-	return exePath, nil
+	err = os.Remove(testTargetDir)
+	if err != nil {
+		return "", err
+	}
+
+	_, err = os.Stat(finalTestDestination)
+	if err != nil {
+		return "", fmt.Errorf("Expected '%s' to exist, %w", finalTestDestination, err)
+	}
+	return finalTestDestination, nil
 }
 
 // runs a soak test based on the tag, launching as many chainlink nodes as necessary
 func RunSoakTest(testTag, namespacePrefix string, chainlinkReplicas int) error {
-	LoadConfigs()
 	soakTestsPath := filepath.Join(utils.SoakRoot, "tests")
 	exePath, err := BuildGoTests(utils.ProjectRoot, soakTestsPath)
 	if err != nil {
