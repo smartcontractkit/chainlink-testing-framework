9c9c4519ee70cd6e2155b3e658c5d17e89d3a41b
AnieeG <anindita.ghosh@smartcontract.com>
2022-07-20 17:23:05 -0700

    repackaging

--- testreporters/reporter_model.go
+++ testreporters/reporter_model.go
@@ -1,42 +1,39 @@
-package actions
+package testreporters
 
 import (
 	"fmt"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/rs/zerolog/log"
+	"github.com/slack-go/slack"
+	"github.com/smartcontractkit/chainlink-env/environment"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"path/filepath"
 	"strings"
 	"time"
-
-	"github.com/onsi/ginkgo/v2"
-	"github.com/onsi/gomega"
-	"github.com/rs/zerolog/log"
-
-	"github.com/smartcontractkit/chainlink-env/environment"
-	"github.com/smartcontractkit/chainlink-testing-framework/logging"
-	"github.com/smartcontractkit/chainlink-testing-framework/testreporters"
-	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
 )
+
+// TestReporter is a general interface for all test reporters
+type TestReporter interface {
+	WriteReport(folderLocation string) error
+	SendSlackNotification(slackClient *slack.Client) error
+	SetNamespace(namespace string)
+}
 
 const (
 	// DefaultArtifactsDir default artifacts dir
 	DefaultArtifactsDir string = "logs"
 )
 
-// GinkgoSuite provides the default setup for running a Ginkgo test suite
-func GinkgoSuite() {
-	logging.Init()
-	gomega.RegisterFailHandler(ginkgo.Fail)
-}
-
 // attempts to download the logs of all ephemeral test deployments onto the test runner, also writing a test report
 // if one is provided
-func WriteTeardownLogs(env *environment.Environment, optionalTestReporter testreporters.TestReporter) error {
+func WriteTeardownLogs(env *environment.Environment, optionalTestReporter TestReporter) error {
 	if ginkgo.CurrentSpecReport().Failed() || optionalTestReporter != nil {
 		testFilename := strings.Split(ginkgo.CurrentSpecReport().FileName(), ".")[0]
 		_, testName := filepath.Split(testFilename)
 		logsPath := filepath.Join(DefaultArtifactsDir, fmt.Sprintf("%s-%d", testName, time.Now().Unix()))
 		if err := env.Artifacts.DumpTestResult(logsPath, "chainlink"); err != nil {
 			log.Warn().Err(err).Msg("Error trying to collect pod logs")
-			if kubeerrors.IsForbidden(err) {
+			if errors.IsForbidden(err) {
 				log.Warn().Msg("Unable to gather logs from a remote_test_runner instance. Working on improving this.")
 			} else {
 				return err
@@ -50,7 +47,7 @@ func WriteTeardownLogs(env *environment.Environment, optionalTestReporter testre
 }
 
 // if provided, writes a test report and sends a Slack notification
-func SendReport(env *environment.Environment, logsPath string, optionalTestReporter testreporters.TestReporter) error {
+func SendReport(env *environment.Environment, logsPath string, optionalTestReporter TestReporter) error {
 	if optionalTestReporter != nil {
 		log.Info().Msg("Writing Test Report")
 		optionalTestReporter.SetNamespace(env.Cfg.Namespace)
