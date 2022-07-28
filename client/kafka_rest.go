63ea4fd37e5d5d4bfb6bfaebbc2b8ff3cc5a75af
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2022-07-28 08:17:39 -0700

    Wrap api calls with go-resty library (#392)

--- client/kafka_rest.go
+++ client/kafka_rest.go
@@ -1,11 +1,16 @@
 package client
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
+)
 
 // KafkaRestClient kafka-rest client
 type KafkaRestClient struct {
-	*BasicHTTPClient
-	Config *KafkaRestConfig
+	APIClient *resty.Client
+	Config    *KafkaRestConfig
 }
 
 // KafkaRestConfig holds config information for KafkaRestClient
@@ -16,14 +21,17 @@ type KafkaRestConfig struct {
 // NewKafkaRestClient creates a new KafkaRestClient
 func NewKafkaRestClient(cfg *KafkaRestConfig) *KafkaRestClient {
 	return &KafkaRestClient{
-		Config:          cfg,
-		BasicHTTPClient: NewBasicHTTPClient(&http.Client{}, cfg.URL),
+		Config:    cfg,
+		APIClient: resty.New().SetBaseURL(cfg.URL),
 	}
 }
 
 // GetTopics Get a list of Kafka topics.
 func (krc *KafkaRestClient) GetTopics() ([]string, error) {
 	responseBody := []string{}
-	_, err := krc.Do(http.MethodGet, "/topics", nil, &responseBody, http.StatusOK)
+	resp, err := krc.APIClient.R().SetResult(responseBody).Get("/topics")
+	if resp.StatusCode() != http.StatusOK {
+		err = fmt.Errorf("Unexpected Status Code. Expected %d; Got %d", http.StatusOK, resp.StatusCode())
+	}
 	return responseBody, err
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/ethereum/go-ethereum v1.10.18
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/google/uuid v1.3.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
