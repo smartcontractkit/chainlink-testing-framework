f4186e8df3dcf8c0cbec6b1b379bf9ee84590da9
Adam Hamrick <adam.hamrick@smartcontract.com>
2022-08-01 15:20:15 -0400

    MVP

--- blockchain/arbitrum.go
+++ blockchain/arbitrum.go
@@ -23,28 +23,28 @@ type ArbitrumClient struct {
 }
 
 // Fund sends some ETH to an address using the default wallet
-func (m *ArbitrumClient) Fund(toAddress string, amount *big.Float) error {
-	privateKey, err := crypto.HexToECDSA(m.DefaultWallet.PrivateKey())
+func (a *ArbitrumClient) Fund(toAddress string, amount *big.Float) error {
+	privateKey, err := crypto.HexToECDSA(a.DefaultWallet.PrivateKey())
 	to := common.HexToAddress(toAddress)
 	if err != nil {
 		return fmt.Errorf("invalid private key: %v", err)
 	}
 	// Metis uses legacy transactions and gas estimations, is behind London fork as of 04/27/2022
-	suggestedGasPrice, err := m.Client.SuggestGasPrice(context.Background())
+	suggestedGasPrice, err := a.Client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return err
 	}
 
 	// Bump gas price
-	gasPriceBuffer := big.NewInt(0).SetUint64(m.NetworkConfig.GasEstimationBuffer)
+	gasPriceBuffer := big.NewInt(0).SetUint64(a.NetworkConfig.GasEstimationBuffer)
 	suggestedGasPrice.Add(suggestedGasPrice, gasPriceBuffer)
 
-	nonce, err := m.GetNonce(context.Background(), common.HexToAddress(m.DefaultWallet.Address()))
+	nonce, err := a.GetNonce(context.Background(), common.HexToAddress(a.DefaultWallet.Address()))
 	if err != nil {
 		return err
 	}
 
-	tx, err := types.SignNewTx(privateKey, types.LatestSignerForChainID(m.GetChainID()), &types.LegacyTx{
+	tx, err := types.SignNewTx(privateKey, types.LatestSignerForChainID(a.GetChainID()), &types.LegacyTx{
 		Nonce:    nonce,
 		To:       &to,
 		Value:    utils.EtherToWei(amount),
@@ -57,13 +57,42 @@ func (m *ArbitrumClient) Fund(toAddress string, amount *big.Float) error {
 
 	log.Info().
 		Str("Token", "ARB").
-		Str("From", m.DefaultWallet.Address()).
+		Str("From", a.DefaultWallet.Address()).
 		Str("To", toAddress).
 		Str("Amount", amount.String()).
 		Msg("Funding Address")
-	if err := m.Client.SendTransaction(context.Background(), tx); err != nil {
+	if err := a.Client.SendTransaction(context.Background(), tx); err != nil {
 		return err
 	}
 
-	return m.ProcessTransaction(tx)
+	return a.ProcessTransaction(tx)
+}
+
+// ProcessTransaction adds tx hash to a separate list to check for on waiting. Arbitrum is a near-instant
+// Optimistic Rollup that requires we keep MinConfirmations = 0 in most cases, but can take up to a couple seconds to
+// actually confirm. IsPending also seems to be improperly implemented in Arbitrum, so need a separate strategy to
+// confirm transactions.
+func (a *ArbitrumClient) IsTxConfirmed(txHash common.Hash) (bool, error) {
+	tx, _, err := a.Client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		return false, err
+	}
+	receipt, err := a.Client.TransactionReceipt(context.Background(), txHash)
+	if err != nil {
+		return false, err
+	}
+	a.gasStats.AddClientTXData(TXGasData{
+		TXHash:            txHash.String(),
+		Value:             tx.Value().Uint64(),
+		GasLimit:          tx.Gas(),
+		GasUsed:           receipt.GasUsed,
+		GasPrice:          tx.GasPrice().Uint64(),
+		CumulativeGasUsed: receipt.CumulativeGasUsed,
+	})
+	latestBlockNum, err := a.Client.BlockNumber(context.Background())
+	if err != nil {
+		return false, err
+	}
+
+	return receipt.BlockNumber.Uint64() >= latestBlockNum, err
 }
