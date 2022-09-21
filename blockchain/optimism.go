835f4442d79f8736a7202e4008f5faa15f98888c
Adam Hamrick <adam.hamrick@smartcontract.com>
2022-09-21 15:47:01 -0400

    Optimisim Contracts (#445)

--- blockchain/optimism.go
+++ blockchain/optimism.go
@@ -24,24 +24,24 @@ type OptimismClient struct {
 }
 
 // Fund sends some OP to an address using the default wallet
-func (m *OptimismClient) Fund(toAddress string, amount *big.Float) error {
-	privateKey, err := crypto.HexToECDSA(m.DefaultWallet.PrivateKey())
+func (o *OptimismClient) Fund(toAddress string, amount *big.Float) error {
+	privateKey, err := crypto.HexToECDSA(o.DefaultWallet.PrivateKey())
 	to := common.HexToAddress(toAddress)
 	if err != nil {
 		return fmt.Errorf("invalid private key: %v", err)
 	}
 	// Optimism uses legacy transactions and gas estimations
-	suggestedGasPrice, err := m.Client.SuggestGasPrice(context.Background())
+	suggestedGasPrice, err := o.Client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return err
 	}
 
-	nonce, err := m.GetNonce(context.Background(), common.HexToAddress(m.DefaultWallet.Address()))
+	nonce, err := o.GetNonce(context.Background(), common.HexToAddress(o.DefaultWallet.Address()))
 	if err != nil {
 		return err
 	}
 
-	tx, err := types.SignNewTx(privateKey, types.LatestSignerForChainID(m.GetChainID()), &types.LegacyTx{
+	tx, err := types.SignNewTx(privateKey, types.LatestSignerForChainID(o.GetChainID()), &types.LegacyTx{
 		Nonce:    nonce,
 		To:       &to,
 		Value:    utils.EtherToWei(amount),
@@ -54,13 +54,54 @@ func (m *OptimismClient) Fund(toAddress string, amount *big.Float) error {
 
 	log.Info().
 		Str("Token", "OP").
-		Str("From", m.DefaultWallet.Address()).
+		Str("From", o.DefaultWallet.Address()).
 		Str("To", toAddress).
 		Str("Amount", amount.String()).
 		Msg("Funding Address")
-	if err := m.Client.SendTransaction(context.Background(), tx); err != nil {
+	if err := o.Client.SendTransaction(context.Background(), tx); err != nil {
 		return err
 	}
 
-	return m.ProcessTransaction(tx)
+	return o.ProcessTransaction(tx)
+}
+
+// DeployContract deploys smart contracts specifically on Optimism
+func (o *OptimismClient) DeployContract(
+	contractName string,
+	deployer ContractDeployer,
+) (*common.Address, *types.Transaction, interface{}, error) {
+	opts, err := o.TransactionOpts(o.DefaultWallet)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// Optimism uses legacy transactions and gas estimations, is behind London fork as of 04/27/2022
+	suggestedGasPrice, err := o.Client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// Bump gas price
+	gasPriceBuffer := big.NewInt(0).SetUint64(o.NetworkConfig.GasEstimationBuffer)
+	suggestedGasPrice.Add(suggestedGasPrice, gasPriceBuffer)
+
+	opts.GasPrice = suggestedGasPrice
+
+	contractAddress, transaction, contractInstance, err := deployer(opts, o.Client)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	if err = o.ProcessTransaction(transaction); err != nil {
+		return nil, nil, nil, err
+	}
+
+	log.Info().
+		Str("Contract Address", contractAddress.Hex()).
+		Str("Contract Name", contractName).
+		Str("From", o.DefaultWallet.Address()).
+		Str("Total Gas Cost (OP)", utils.WeiToEther(transaction.Cost()).String()).
+		Str("Network Name", o.NetworkConfig.Name).
+		Msg("Deployed contract")
+	return &contractAddress, transaction, contractInstance, err
 }
