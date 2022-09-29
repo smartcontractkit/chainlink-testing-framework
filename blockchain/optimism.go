a22a47227605c882b349a4d31415f5eacae2eee2
Adam Hamrick <adam.hamrick@smartcontract.com>
2022-09-29 18:01:48 -0400

    Remove debugging

--- blockchain/optimism.go
+++ blockchain/optimism.go
@@ -130,10 +130,6 @@ func (o *OptimismClient) ReturnFunds(fromPrivateKey *ecdsa.PrivateKey) error {
 	estimatedGasCost := big.NewInt(1).Mul(suggestedGasPrice, big.NewInt(21000))
 	// Optimism needs to calculate both the L1 and L2 gas fees
 	// https://community.optimism.io/docs/developers/build/transaction-fees/#the-l1-data-fee
-	log.Warn().
-		Uint64("estimatedGasCost", estimatedGasCost.Uint64()).
-		Uint64("balance", balance.Uint64()).
-		Msg("LEVEL 1")
 	optimismL1GasContract := common.HexToAddress("0x420000000000000000000000000000000000000F")
 	optimismGasContract, err := contracts.NewOptimismGas(optimismL1GasContract, o.Client)
 	if err != nil {
@@ -143,11 +139,6 @@ func (o *OptimismClient) ReturnFunds(fromPrivateKey *ecdsa.PrivateKey) error {
 	if err != nil {
 		return err
 	}
-	log.Warn().
-		Uint64("l1Fee", l1Fee.Uint64()).
-		Uint64("balance", balance.Uint64()).
-		Uint64("estimatedGasCost", estimatedGasCost.Uint64()).
-		Msg("LEVEL 2")
 	// Optimism's L1 gas estimation has an error margin of 25%, we use 26 for rounding errors
 	// https://help.optimism.io/hc/en-us/articles/4416677738907
 	l1FeeFloat := big.NewFloat(1).SetUint64(l1Fee.Uint64())
@@ -158,11 +149,6 @@ func (o *OptimismClient) ReturnFunds(fromPrivateKey *ecdsa.PrivateKey) error {
 	estimatedGasCost.Add(estimatedGasCost, l1Fee)
 	estimatedGasCost.Add(estimatedGasCost, big.NewInt(111_677_895_522))
 	balance.Sub(balance, estimatedGasCost)
-	log.Warn().
-		Uint64("l1Fee", l1Fee.Uint64()).
-		Uint64("balance", balance.Uint64()).
-		Uint64("estimatedGasCost", estimatedGasCost.Uint64()).
-		Msg("LEVEL 3")
 
 	nonce, err := o.GetNonce(context.Background(), fromAddress)
 	if err != nil {
