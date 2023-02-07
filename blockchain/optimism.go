d889c54e92526e3b130e8b4cd7fbd7445ec0d99d
Adam Hamrick <adam.hamrick@smartcontract.com>
2023-02-06 21:42:20 -0500

    lint

--- blockchain/optimism.go
+++ blockchain/optimism.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/rs/zerolog/log"
+
 	"github.com/smartcontractkit/chainlink-testing-framework/utils"
 )
 
