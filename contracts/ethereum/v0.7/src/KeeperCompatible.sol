401663072d3e27710674be08adc5bf54853c4e14
anirudhwarrier <anirudhwarrier@users.noreply.github.com>
2023-03-20 17:16:47 +0530

    remove automation contracts

--- contracts/ethereum/v0.7/src/KeeperCompatible.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.7.6;
-
-import './KeeperBase.sol';
-import './KeeperCompatibleInterface.sol';
-
-abstract contract KeeperCompatible is KeeperBase, KeeperCompatibleInterface {}
