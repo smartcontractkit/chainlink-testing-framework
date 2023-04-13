1a3c89802d19adafd31b42b41d306ebd774b3f4a
Adam Hamrick <adam.hamrick@smartcontract.com>
2023-04-13 11:56:59 -0400

    Legacy

--- blockchain/quorum.go
+++ blockchain/quorum.go
@@ -3,6 +3,7 @@ package blockchain
 import (
 	"context"
 	"crypto/ecdsa"
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum"
@@ -26,141 +27,138 @@ type QuorumClient struct {
 	*EthereumClient
 }
 
-// Fund overrides ethereum's fund to account for Quorum's gas specifications
-func (k *QuorumClient) Fund(
-	toAddress string,
-	amount *big.Float,
-) error {
-	privateKey, err := crypto.HexToECDSA(k.DefaultWallet.PrivateKey())
+// Fund sends some ETH to an address using the default wallet
+func (r *QuorumClient) Fund(toAddress string, amount *big.Float) error {
+	privateKey, err := crypto.HexToECDSA(r.DefaultWallet.PrivateKey())
 	to := common.HexToAddress(toAddress)
 	if err != nil {
-		return err
+		return fmt.Errorf("invalid private key: %v", err)
 	}
-	// Don't bump gas for Quorum
-	gasPrice, err := k.Client.SuggestGasPrice(context.Background())
+	// Quorum uses legacy transactions and gas estimations,
+	suggestedGasPrice, err := r.Client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return err
 	}
-	nonce, err := k.GetNonce(context.Background(), k.DefaultWallet.address)
+
+	// Bump gas price
+	gasPriceBuffer := big.NewInt(0).SetUint64(r.NetworkConfig.GasEstimationBuffer)
+	suggestedGasPrice.Add(suggestedGasPrice, gasPriceBuffer)
+
+	nonce, err := r.GetNonce(context.Background(), common.HexToAddress(r.DefaultWallet.Address()))
 	if err != nil {
 		return err
 	}
-	log.Warn().
-		Str("Network Name", k.NetworkConfig.Name).
-		Msg("Setting GasTipCap = SuggestedGasPrice for Quorum network")
-	estimatedGas, err := k.Client.EstimateGas(context.Background(), ethereum.CallMsg{})
+	estimatedGas, err := r.Client.EstimateGas(context.Background(), ethereum.CallMsg{})
 	if err != nil {
 		return err
 	}
-	// https://docs.Quorum.com/Quorum/design/transaction-fees#gas
-	tx, err := types.SignNewTx(privateKey, types.LatestSignerForChainID(k.GetChainID()), &types.DynamicFeeTx{
-		ChainID:   k.GetChainID(),
-		Nonce:     nonce,
-		To:        &to,
-		Value:     utils.EtherToWei(amount),
-		GasTipCap: gasPrice,
-		GasFeeCap: gasPrice,
-		Gas:       estimatedGas,
+
+	tx, err := types.SignNewTx(privateKey, types.LatestSignerForChainID(r.GetChainID()), &types.LegacyTx{
+		Nonce:    nonce,
+		To:       &to,
+		Value:    utils.EtherToWei(amount),
+		GasPrice: suggestedGasPrice,
+		Gas:      estimatedGas,
 	})
 	if err != nil {
 		return err
 	}
 
 	log.Info().
-		Str("Token", "Q").
-		Str("From", k.DefaultWallet.Address()).
+		Str("Token", "Quorum").
+		Str("From", r.DefaultWallet.Address()).
 		Str("To", toAddress).
 		Str("Amount", amount.String()).
-		Uint64("Estimated Gas Cost", new(big.Int).Mul(gasPrice, new(big.Int).SetUint64(estimatedGas)).Uint64()).
+		Uint64("Estimated Gas Cost", new(big.Int).Mul(suggestedGasPrice, new(big.Int).SetUint64(estimatedGas)).Uint64()).
 		Msg("Funding Address")
-	if err := k.SendTransaction(context.Background(), tx); err != nil {
+	if err := r.SendTransaction(context.Background(), tx); err != nil {
 		return err
 	}
-	return k.ProcessTransaction(tx)
+
+	return r.ProcessTransaction(tx)
 }
 
-// DeployContract acts as a general contract deployment tool to an ethereum chain
-func (k *QuorumClient) DeployContract(
+// DeployContract acts as a general contract deployment tool to an EVM chain
+func (r *QuorumClient) DeployContract(
 	contractName string,
 	deployer ContractDeployer,
 ) (*common.Address, *types.Transaction, interface{}, error) {
-	opts, err := k.TransactionOpts(k.DefaultWallet)
+	opts, err := r.TransactionOpts(r.DefaultWallet)
 	if err != nil {
 		return nil, nil, nil, err
 	}
 
-	// Don't bump gas for Quorum
-	// https://docs.Quorum.com/Quorum/design/transaction-fees#unit-price
-	opts.GasTipCap = nil
-	opts.GasPrice = nil
-
-	contractAddress, transaction, contractInstance, err := deployer(opts, k.Client)
+	// Quorum uses legacy transactions and gas estimations, is behind London fork as of 04/27/2022
+	opts.GasPrice, err = r.Client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return nil, nil, nil, err
 	}
 
-	if err = k.ProcessTransaction(transaction); err != nil {
+	contractAddress, transaction, contractInstance, err := deployer(opts, r.Client)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	if err = r.ProcessTransaction(transaction); err != nil {
 		return nil, nil, nil, err
 	}
 
 	log.Info().
 		Str("Contract Address", contractAddress.Hex()).
 		Str("Contract Name", contractName).
-		Str("From", k.DefaultWallet.Address()).
-		Str("Total Gas Cost (Q)", utils.WeiToEther(transaction.Cost()).String()).
-		Str("Network Name", k.NetworkConfig.Name).
+		Str("From", r.DefaultWallet.Address()).
+		Str("Total Gas Cost (Quorum)", utils.WeiToEther(transaction.Cost()).String()).
+		Str("Network Name", r.NetworkConfig.Name).
 		Msg("Deployed contract")
 	return &contractAddress, transaction, contractInstance, err
 }
 
-func (k *QuorumClient) ReturnFunds(fromPrivateKey *ecdsa.PrivateKey) error {
-	to := common.HexToAddress(k.DefaultWallet.Address())
+func (r *QuorumClient) ReturnFunds(fromPrivateKey *ecdsa.PrivateKey) error {
+	to := common.HexToAddress(r.DefaultWallet.Address())
 
-	// Don't bump gas for Quorum
-	gasPrice, err := k.Client.SuggestGasPrice(context.Background())
+	suggestedGasPrice, err := r.Client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return err
 	}
-
 	fromAddress, err := utils.PrivateKeyToAddress(fromPrivateKey)
 	if err != nil {
 		return err
 	}
 
-	nonce, err := k.GetNonce(context.Background(), fromAddress)
+	balance, err := r.Client.BalanceAt(context.Background(), fromAddress, nil)
 	if err != nil {
 		return err
 	}
-	balance, err := k.Client.BalanceAt(context.Background(), fromAddress, nil)
+	balance.Sub(balance, big.NewInt(1).Mul(suggestedGasPrice, big.NewInt(21000)))
+
+	nonce, err := r.GetNonce(context.Background(), fromAddress)
 	if err != nil {
 		return err
 	}
-	estimatedGas, err := k.Client.EstimateGas(context.Background(), ethereum.CallMsg{})
+	estimatedGas, err := r.Client.EstimateGas(context.Background(), ethereum.CallMsg{})
 	if err != nil {
 		return err
 	}
-	balance.Sub(balance, big.NewInt(1).Mul(gasPrice, big.NewInt(0).SetUint64(estimatedGas)))
-	// https://docs.Quorum.com/Quorum/design/transaction-fees#gas
-	tx, err := types.SignNewTx(fromPrivateKey, types.LatestSignerForChainID(k.GetChainID()), &types.DynamicFeeTx{
-		ChainID:   k.GetChainID(),
-		Nonce:     nonce,
-		To:        &to,
-		Value:     balance,
-		GasTipCap: gasPrice,
-		GasFeeCap: gasPrice,
-		Gas:       estimatedGas,
+
+	tx, err := types.SignNewTx(fromPrivateKey, types.LatestSignerForChainID(r.GetChainID()), &types.LegacyTx{
+		Nonce:    nonce,
+		To:       &to,
+		Value:    balance,
+		GasPrice: suggestedGasPrice,
+		Gas:      estimatedGas,
 	})
 	if err != nil {
 		return err
 	}
 
 	log.Info().
-		Str("Token", "Q").
+		Str("Token", "Quorum").
 		Str("From", fromAddress.Hex()).
 		Str("Amount", balance.String()).
 		Msg("Returning Funds to Default Wallet")
-	if err := k.SendTransaction(context.Background(), tx); err != nil {
+	if err := r.SendTransaction(context.Background(), tx); err != nil {
 		return err
 	}
-	return k.ProcessTransaction(tx)
+
+	return r.ProcessTransaction(tx)
 }
