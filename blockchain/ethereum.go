964ddc9a1710a3dcd22cef1a51ddaa9632348dc8
AnieeG <anindita.ghosh@smartcontract.com>
2023-05-16 08:02:13 -0700

    comments

--- blockchain/ethereum.go
+++ blockchain/ethereum.go
@@ -831,10 +831,12 @@ func NewEVMClient(networkSettings EVMNetwork, env *environment.Environment) (EVM
 	return wrappedClient, nil
 }
 
-// ConcurrentEVMClient returns a multi-node EVM client connected to the specified network
+// ConcurrentEVMClient returns a multi-node EVM client connected to a specified network
 // It is used for concurrent interactions from different threads with the same network and from same owner
 // account. This ensures that correct nonce value is fetched when an instance of EVMClient is initiated using this method.
+// This is mainly useful for simulated networks as we don't use global nonce manager for them.
 func ConcurrentEVMClient(networkSettings EVMNetwork, env *environment.Environment, existing EVMClient) (EVMClient, error) {
+	// if not simulated use the NewEVMClient
 	if !networkSettings.Simulated {
 		return NewEVMClient(networkSettings, env)
 	}
@@ -860,19 +862,7 @@ func ConcurrentEVMClient(networkSettings EVMNetwork, env *environment.Environmen
 
 	ecl.DefaultClient = ecl.Clients[0]
 	wrappedClient := wrapMultiClient(networkSettings, ecl)
-
-	if ecl.NetworkSimulated() {
-		return wrappedClient, nil
-	}
-	fundingBalance, err := wrappedClient.BalanceAt(context.Background(), wrappedClient.GetDefaultWallet().address)
-	if err != nil {
-		return nil, err
-	}
-	log.Debug().
-		Str("Address", wrappedClient.GetDefaultWallet().address.Hex()).
-		Uint64("Balance", fundingBalance.Uint64()).
-		Msg("Funding Wallet")
-
+	// no need to fund the account as it is already funded in the existing client
 	return wrappedClient, nil
 }
 
