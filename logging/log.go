73415ce1f30ea9f864effa85d1063526582f500a
Adam Hamrick <adam.hamrick@smartcontract.com>
2023-07-26 10:59:09 -0400

    Logging

--- logging/log.go
+++ logging/log.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -16,5 +17,6 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
+	zerolog.TimeFieldFormat = time.RFC3339Nano
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "15:04:05.00"}).Level(lvl).With().Timestamp().Logger()
 }
