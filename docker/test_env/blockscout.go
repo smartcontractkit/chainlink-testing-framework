fbc0df5a21ce5fbfd26b1fac21047cf15bab111c
AnieeG <anindita.ghosh@smartcontract.com>
2023-08-21 12:37:13 -0700

    added blockscout

---
+++ docker/test_env/blockscout.go
@@ -0,0 +1,101 @@
+package test_env
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/rs/zerolog/log"
+	tc "github.com/testcontainers/testcontainers-go"
+	tcwait "github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/smartcontractkit/chainlink-testing-framework/blockchain"
+)
+
+const (
+	BLKSCOUT_IMAGE     = "f4hrenh9it/blockscout:v1"
+	BLKSCOUT_HTTP_PORT = "4000"
+	BLKSCOUT_PASS      = "blockscoutpass"
+	BLCSCOUT_DB_NAME   = "blockscout"
+)
+
+type Blockscout struct {
+	EnvComponent
+	NetworkConfig blockchain.EVMNetwork
+	HTTPURL       string
+	pgContainer   *PostgresDb
+}
+
+func NewBlockscout(networks []string, networkConfig blockchain.EVMNetwork) *Blockscout {
+	return &Blockscout{
+		EnvComponent: EnvComponent{
+			ContainerName: fmt.Sprintf("%s-%s", "blockscout", networkConfig.Name),
+			Networks:      networks,
+		},
+		NetworkConfig: networkConfig,
+	}
+}
+
+func (b *Blockscout) Start() error {
+	b.pgContainer = NewPostgresDb(b.Networks,
+		WithPostgresDbContainerName(fmt.Sprintf("pg-%s", b.ContainerName)),
+		WithPostgresDbDatabaseName(BLCSCOUT_DB_NAME),
+		WithPostgresDbPassword(BLKSCOUT_PASS),
+	)
+	err := b.pgContainer.StartContainer()
+	if err != nil {
+		return err
+	}
+	req := b.getBlockscoutContainerRequest()
+	blockscout, err := tc.GenericContainer(context.Background(),
+		tc.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		})
+	if err != nil {
+		return err
+	}
+	host, err := blockscout.Host(context.Background())
+	if err != nil {
+		return err
+	}
+	port, err := blockscout.MappedPort(context.Background(), BLKSCOUT_HTTP_PORT)
+	if err != nil {
+		return err
+	}
+	b.HTTPURL = fmt.Sprintf("http://%s:%s", host, port.Port())
+	log.Info().Str("url", b.HTTPURL).Msg("Blockscout started")
+	return nil
+}
+
+func (b *Blockscout) getBlockscoutContainerRequest() tc.ContainerRequest {
+	return tc.ContainerRequest{
+		Name:  b.ContainerName,
+		Image: BLKSCOUT_IMAGE,
+		ExposedPorts: []string{
+			fmt.Sprintf("%s/tcp", BLKSCOUT_HTTP_PORT),
+		},
+		Networks: b.Networks,
+		WaitingFor: tcwait.ForHTTP("/").
+			WithPort(nat.Port(fmt.Sprintf("%s/tcp", BLKSCOUT_HTTP_PORT))),
+		Entrypoint: []string{"/bin/bash", "-c",
+			"mix", "ecto.create", "&&",
+			"mix", "ecto.migrate", "&&",
+			"mix", "phx.server",
+		},
+		Env: map[string]string{
+			"MIX_ENV":                   "prod",
+			"ECTO_USE_SSL":              "'false'",
+			"COIN":                      "DAI",
+			"ETHEREUM_JSONRPC_VARIANT":  "geth",
+			"ETHEREUM_JSONRPC_HTTP_URL": b.NetworkConfig.HTTPURLs[0],
+			"ETHEREUM_JSONRPC_WS_URL":   b.NetworkConfig.URLs[0],
+			"DATABASE_URL": fmt.Sprintf(
+				"postgresql://%s:%s@%s:5432/%s?ssl=false",
+				b.pgContainer.User,
+				b.pgContainer.Password,
+				b.pgContainer.ContainerName,
+				b.pgContainer.DbName),
+		},
+	}
+}
