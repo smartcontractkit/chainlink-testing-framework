b2e4d040466ee34c8272cabb6127b63600b3fa1f
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2023-10-23 13:35:37 -0700

    fix concurrent client (#745)

--- blockchain/ethereum.go
+++ blockchain/ethereum.go
@@ -1215,7 +1215,7 @@ func ConnectEVMClient(networkSettings EVMNetwork, logger zerolog.Logger) (EVMCli
 func ConcurrentEVMClient(networkSettings EVMNetwork, env *environment.Environment, existing EVMClient, logger zerolog.Logger) (EVMClient, error) {
 	// if not simulated use the NewEVMClient
 	if !networkSettings.Simulated {
-		return NewEVMClient(networkSettings, env, logger)
+		return ConnectEVMClient(networkSettings, logger)
 	}
 	ecl := &EthereumMultinodeClient{}
 	if env != nil {
