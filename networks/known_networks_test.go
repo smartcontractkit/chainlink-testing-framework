3868cecf73ccee3d741198be420a6d40cce1041e
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2023-10-27 16:44:41 +0200

    Use t.Setenv

--- networks/known_networks_test.go
+++ networks/known_networks_test.go
@@ -17,21 +17,17 @@ func TestMain(m *testing.M) {
 }
 
 func TestMustGetSelectedNetworksFromEnv_Missing_SELECTED_NETWORKS(t *testing.T) {
-	defer os.Clearenv()
-
 	require.Panics(t, func() {
 		MustGetSelectedNetworksFromEnv()
 	})
 }
 
 func TestMustGetSelectedNetworksFromEnv_Missing_HTTP_URLS(t *testing.T) {
-	defer os.Clearenv()
-
 	networkKey := "ARBITRUM_GOERLI"
 
-	os.Setenv("SELECTED_NETWORKS", networkKey)
-	os.Setenv(fmt.Sprintf("%s_URLS", networkKey), "xxxx")
-	os.Setenv(fmt.Sprintf("%s_KEYS", networkKey), "xxxx")
+	t.Setenv("SELECTED_NETWORKS", networkKey)
+	t.Setenv(fmt.Sprintf("%s_URLS", networkKey), "xxxx")
+	t.Setenv(fmt.Sprintf("%s_KEYS", networkKey), "xxxx")
 
 	require.PanicsWithError(t, fmt.Sprintf("set %s_HTTP_URLS env var", networkKey), func() {
 		MustGetSelectedNetworksFromEnv()
@@ -39,13 +35,11 @@ func TestMustGetSelectedNetworksFromEnv_Missing_HTTP_URLS(t *testing.T) {
 }
 
 func TestMustGetSelectedNetworksFromEnv_Missing_KEYS(t *testing.T) {
-	defer os.Clearenv()
-
 	networkKey := "ARBITRUM_GOERLI"
 
-	os.Setenv("SELECTED_NETWORKS", networkKey)
-	os.Setenv(fmt.Sprintf("%s_URLS", networkKey), "xxxx")
-	os.Setenv(fmt.Sprintf("%s_HTTP_URLS", networkKey), "xxxx")
+	t.Setenv("SELECTED_NETWORKS", networkKey)
+	t.Setenv(fmt.Sprintf("%s_URLS", networkKey), "xxxx")
+	t.Setenv(fmt.Sprintf("%s_HTTP_URLS", networkKey), "xxxx")
 
 	require.PanicsWithError(t, fmt.Sprintf("set %s_KEYS env var", networkKey), func() {
 		MustGetSelectedNetworksFromEnv()
@@ -53,13 +47,11 @@ func TestMustGetSelectedNetworksFromEnv_Missing_KEYS(t *testing.T) {
 }
 
 func TestMustGetSelectedNetworksFromEnv_Missing_URLS(t *testing.T) {
-	defer os.Clearenv()
-
 	networkKey := "ARBITRUM_GOERLI"
 
-	os.Setenv("SELECTED_NETWORKS", networkKey)
-	os.Setenv(fmt.Sprintf("%s_HTTP_URLS", networkKey), "xxxx")
-	os.Setenv(fmt.Sprintf("%s_KEYS", networkKey), "xxxx")
+	t.Setenv("SELECTED_NETWORKS", networkKey)
+	t.Setenv(fmt.Sprintf("%s_HTTP_URLS", networkKey), "xxxx")
+	t.Setenv(fmt.Sprintf("%s_KEYS", networkKey), "xxxx")
 
 	require.PanicsWithError(t, fmt.Sprintf("set %s_URLS env var", networkKey), func() {
 		MustGetSelectedNetworksFromEnv()
@@ -67,15 +59,13 @@ func TestMustGetSelectedNetworksFromEnv_Missing_URLS(t *testing.T) {
 }
 
 func TestMustGetSelectedNetworksFromEnv_MultipleNetworks(t *testing.T) {
-	defer os.Clearenv()
-
 	networkKey := "ARBITRUM_GOERLI,OPTIMISM_GOERLI"
-	os.Setenv("SELECTED_NETWORKS", networkKey)
+	t.Setenv("SELECTED_NETWORKS", networkKey)
 
 	for _, network := range strings.Split(networkKey, ",") {
-		os.Setenv(fmt.Sprintf("%s_URLS", network), "wss://devnet-1.mt/ABC/rpc/")
-		os.Setenv(fmt.Sprintf("%s_HTTP_URLS", network), "https://devnet-1.mt/ABC/rpc/")
-		os.Setenv(fmt.Sprintf("%s_KEYS", network), "1810868fc221b9f50b5b3e0186d8a5f343f892e51ce12a9e818f936ec0b651ed")
+		t.Setenv(fmt.Sprintf("%s_URLS", network), "wss://devnet-1.mt/ABC/rpc/")
+		t.Setenv(fmt.Sprintf("%s_HTTP_URLS", network), "https://devnet-1.mt/ABC/rpc/")
+		t.Setenv(fmt.Sprintf("%s_KEYS", network), "1810868fc221b9f50b5b3e0186d8a5f343f892e51ce12a9e818f936ec0b651ed")
 	}
 
 	networks := MustGetSelectedNetworksFromEnv()
