4a7af19c8b7391576cabe28a8854e08e3c3aa554
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2023-10-27 17:22:19 +0200

    Add tests for NewEVMNetwork

--- networks/known_networks_test.go
+++ networks/known_networks_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/smartcontractkit/chainlink-testing-framework/blockchain"
 	"github.com/smartcontractkit/chainlink-testing-framework/logging"
 )
 
@@ -72,4 +73,39 @@ func TestMustGetSelectedNetworksFromEnv_MultipleNetworks(t *testing.T) {
 	require.Len(t, networks, 2)
 	require.Equal(t, "Arbitrum Goerli", networks[0].Name)
 	require.Equal(t, "Optimism Goerli", networks[1].Name)
+}
+
+func TestNewEVMNetwork(t *testing.T) {
+	// Set up a mock mapping for this test
+	MappedNetworks = map[string]blockchain.EVMNetwork{
+		"VALID_KEY": {
+			HTTPURLs: []string{"default_http"},
+			URLs:     []string{"default_ws"},
+		},
+	}
+
+	t.Run("valid networkKey", func(t *testing.T) {
+		network, err := NewEVMNetwork("VALID_KEY", nil, nil, nil)
+		require.NoError(t, err)
+		require.Equal(t, MappedNetworks["VALID_KEY"].HTTPURLs, network.HTTPURLs)
+		require.Equal(t, MappedNetworks["VALID_KEY"].URLs, network.URLs)
+	})
+
+	t.Run("invalid networkKey", func(t *testing.T) {
+		_, err := NewEVMNetwork("INVALID_KEY", nil, nil, nil)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "network key: 'INVALID_KEY' is invalid")
+	})
+
+	t.Run("overwriting default values", func(t *testing.T) {
+		walletKeys := []string{"1810868fc221b9f50b5b3e0186d8a5f343f892e51ce12a9e818f936ec0b651ed"}
+		httpUrls := []string{"http://newurl.com"}
+		wsUrls := []string{"ws://newwsurl.com"}
+
+		network, err := NewEVMNetwork("VALID_KEY", walletKeys, httpUrls, wsUrls)
+		require.NoError(t, err)
+		require.Equal(t, httpUrls, network.HTTPURLs)
+		require.Equal(t, wsUrls, network.URLs)
+		require.Equal(t, walletKeys, network.PrivateKeys)
+	})
 }
