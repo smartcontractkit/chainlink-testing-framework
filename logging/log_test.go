8e84add24e3990dd8f5d03bf678dce597299942d
Tate <tate.exon@smartcontract.com>
2023-12-13 10:31:19 -0700

    Triage really long logs issues

--- logging/log_test.go
+++ logging/log_test.go
@@ -1,6 +1,7 @@
 package logging
 
 import (
+	"strings"
 	"testing"
 	"time"
 
@@ -26,4 +27,26 @@ func TestGetTestLogger(t *testing.T) {
 func TestGetTestContainersGoTestLogger(t *testing.T) {
 	l := GetTestContainersGoTestLogger(t)
 	require.NotNil(t, l.(CustomT).L)
+}
+
+// TestSplitStringIntoChunks tests the splitStringIntoChunks function with a string up to a million characters.
+func TestSplitStringIntoChunks(t *testing.T) {
+	// Create a test string with a million and 1 characters ('a').
+	testString := strings.Repeat("a", 1000001)
+	chunkSize := 100000
+	expectedNumChunks := 11
+
+	chunks := SplitStringIntoChunks(testString, chunkSize)
+
+	require.Equal(t, expectedNumChunks, len(chunks), "Wrong number of chunks")
+
+	// Check the size of each chunk.
+	for i, chunk := range chunks {
+		require.False(t, i < expectedNumChunks-1 && len(chunk) != chunkSize, "Chunk %d is not of expected size %d", i+1, chunkSize)
+
+		// Check the last chunk size.
+		if i == expectedNumChunks-1 {
+			require.Equal(t, 1, len(chunk), "Last chunk is not of expected size")
+		}
+	}
 }
