8ff9e15721f5794a630093b1f9f242072e78f756
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-01-11 18:33:58 -0300

    [TT-755] grafana url shortener (#811)
    * shorten grafana urls
    * do not expect dashboard url to have any query params

--- logstream/logstream_handlers.go
+++ logstream/logstream_handlers.go
@@ -164,17 +164,26 @@ func (h *LokiLogHandler) GetLogLocation(consumers map[string]*ContainerLogConsum
 	}
 
 	// if no Grafana URL has been set let's at least print query parameters that can be manually added to the dashboard url
-	grafanaDashboardUrl := ""
-	if h.loggingConfig.Grafana.Url != nil {
-		grafanaDashboardUrl = *h.loggingConfig.Grafana.Url
-		grafanaDashboardUrl = strings.TrimSuffix(grafanaDashboardUrl, "/")
+	baseUrl := ""
+	if h.loggingConfig.Grafana != nil && h.loggingConfig.Grafana.BaseUrl != nil {
+		baseUrl = *h.loggingConfig.Grafana.BaseUrl
+		baseUrl = strings.TrimSuffix(baseUrl, "/")
+		baseUrl = baseUrl + "/"
+	}
+
+	dabshoardUrl := ""
+	if h.loggingConfig.Grafana != nil && h.loggingConfig.Grafana.DashboardUrl != nil {
+		dabshoardUrl = *h.loggingConfig.Grafana.DashboardUrl
+		dabshoardUrl = strings.TrimSuffix(dabshoardUrl, "/")
+		dabshoardUrl = strings.TrimPrefix(dabshoardUrl, "/")
 	}
 
 	rangeFrom := time.Now()
 	rangeTo := time.Now().Add(time.Minute) //just to make sure we get the last message
 
 	var sb strings.Builder
-	sb.WriteString(grafanaDashboardUrl)
+	sb.WriteString(dabshoardUrl)
+	sb.WriteString("?orgId=1&")
 	sb.WriteString(fmt.Sprintf("&var-run_id=%s", *h.loggingConfig.RunId))
 
 	var testName string
@@ -196,7 +205,18 @@ func (h *LokiLogHandler) GetLogLocation(consumers map[string]*ContainerLogConsum
 	if testName != "" {
 		sb.WriteString(fmt.Sprintf("&var-test=%s", testName))
 	}
+
+	relativeUrl := sb.String()
+	sb.WriteString(baseUrl)
 	h.grafanaUrl = sb.String()
+
+	// try to shorten the URL only if we have all the required configuration parameters
+	if baseUrl != "" && dabshoardUrl != "" && h.loggingConfig.Grafana.BearerToken != nil {
+		shortened, err := ShortenUrl(baseUrl, relativeUrl, *h.loggingConfig.Grafana.BearerToken)
+		if err == nil {
+			h.grafanaUrl = shortened
+		}
+	}
 
 	return h.grafanaUrl, nil
 }
