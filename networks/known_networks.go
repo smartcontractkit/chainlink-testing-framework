fa53448ae5f1fa0b285bf070d51562627646a625
Ilja Pavlovs <ilja.pavlovs@gmail.com>
2024-01-17 22:43:39 +0200

    TT-823: Adding Nexon DEV chain

--- networks/known_networks.go
+++ networks/known_networks.go
@@ -664,6 +664,20 @@ var (
 		DefaultGasLimit:           6000000,
 	}
 
+	NexonDev blockchain.EVMNetwork = blockchain.EVMNetwork{
+		Name:                      "Nexon Dev",
+		SupportsEIP1559:           true,
+		ClientImplementation:      blockchain.EthereumClientImplementation,
+		ChainID:                   955081,
+		Simulated:                 false,
+		ChainlinkTransactionLimit: 5000,
+		Timeout:                   blockchain.JSONStrDuration{Duration: time.Minute},
+		MinimumConfirmations:      1,
+		GasEstimationBuffer:       1000,
+		FinalityTag:               true,
+		DefaultGasLimit:           6000000,
+	}
+
 	MappedNetworks = map[string]blockchain.EVMNetwork{
 		"SIMULATED":               SimulatedEVM,
 		"SIMULATED_1":             SimulatedEVMNonDev1,
@@ -710,6 +724,7 @@ var (
 		"WEMIX_MAINNET":         WeMixMainnet,
 		"KROMA_SEPOLIA":         KromaSepolia,
 		"KROMA_MAINNET":         KromaMainnet,
+		"NEXON_DEV":             NexonDev,
 	}
 )
 
