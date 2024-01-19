4701bdad09508cc54d97544c6f32a598962595b7
davidcauchi <david.cauchi@smartcontract.com>
2024-01-19 14:29:19 +0100

    Change to instant finality

--- networks/known_networks.go
+++ networks/known_networks.go
@@ -468,7 +468,7 @@ var (
 		Simulated:                 false,
 		ChainlinkTransactionLimit: 5000,
 		Timeout:                   blockchain.JSONStrDuration{Duration: 3 * time.Minute},
-		MinimumConfirmations:      1,
+		MinimumConfirmations:      0,
 		GasEstimationBuffer:       0,
 	}
 
