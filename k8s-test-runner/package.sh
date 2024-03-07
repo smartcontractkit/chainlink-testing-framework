35c6df976c6e3262427543ac94384ffee09816be
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2024-03-07 13:04:55 +0100

    Update package script

--- k8s-test-runner/package.sh
+++ k8s-test-runner/package.sh
@@ -13,14 +13,14 @@ VERSION=$1
 cd cmd/
 
 # Build the binary for Darwin ARM64 and append the version to its name
-GOOS=darwin GOARCH=arm64 go build -o k8s-test-runner-v$VERSION-darwin-arm64
+GOOS=darwin GOARCH=arm64 go build -o k8s-test-runner-darwin-arm64
 
 # Build the binary for Linux AMD64 and append the version to its name
-GOOS=linux GOARCH=amd64 go build -o k8s-test-runner-v$VERSION-linux-amd64
+GOOS=linux GOARCH=amd64 go build -o k8s-test-runner-linux-amd64
 
 # Navigate back to the root directory
 cd ../
 
 # Package the Dockerfile, Helm chart, and both binaries into a tarball, appending the version to the tarball's name
-tar -czvf k8s-test-runner-v$VERSION.tar.gz Dockerfile.testbin chart cmd/k8s-test-runner-v$VERSION-darwin-arm64 cmd/k8s-test-runner-v$VERSION-linux-amd64
+tar -czvf k8s-test-runner-v$VERSION.tar.gz Dockerfile.testbin chart -C cmd k8s-test-runner-darwin-arm64 k8s-test-runner-linux-amd64
 echo "Created k8s-test-runner-v$VERSION.tar.gz"
