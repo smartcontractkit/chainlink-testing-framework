e157aae30877c2660dea7a8c34d23323a0d1dc2e
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2024-03-08 17:02:00 +0100

    Update package script

--- k8s-test-runner/package.sh
+++ k8s-test-runner/package.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-# Check if version argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <version>"
+# Check if sufficient arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <version> <os(darwin|linux)>"
     exit 1
 fi
 
-# Capture the version from the script arguments
+# Capture the version and OS from the script arguments
 VERSION=$1
+OS=$2
+
+# Validate the OS argument
+if [ "$OS" != "darwin" ] && [ "$OS" != "linux" ]; then
+    echo "Invalid OS. Please select either 'darwin' or 'linux'."
+    exit 1
+fi
 
 # Navigate to the cmd directory
 cd cmd/
 
-# Build the binary for Darwin ARM64 and append the version to its name
-GOOS=darwin GOARCH=arm64 go build -o k8s-test-runner-darwin-arm64
+# Initialize binary name
+BINARY_NAME=""
 
-# Build the binary for Linux AMD64 and append the version to its name
-GOOS=linux GOARCH=amd64 go build -o k8s-test-runner-linux-amd64
+# Build the binary based on the selected OS
+if [ "$OS" == "darwin" ]; then
+    GOOS=darwin GOARCH=arm64 go build -o "k8s-test-runner-$OS-arm64"
+    BINARY_NAME="k8s-test-runner-$OS-arm64"
+elif [ "$OS" == "linux" ]; then
+    GOOS=linux GOARCH=amd64 go build -o "k8s-test-runner-$OS-amd64"
+    BINARY_NAME="k8s-test-runner-$OS-amd64"
+fi
 
 # Navigate back to the root directory
 cd ../
 
-# Package the Dockerfile, Helm chart, and both binaries into a tarball, appending the version to the tarball's name
-tar -czvf k8s-test-runner-v$VERSION.tar.gz Dockerfile.testbin chart -C cmd k8s-test-runner-darwin-arm64 k8s-test-runner-linux-amd64
-echo "Created k8s-test-runner-v$VERSION.tar.gz"
+# Package the Dockerfile, Helm chart, and the selected binary into a tarball, appending the version and OS to the tarball's name
+tar -czvf "k8s-test-runner-$OS-v$VERSION.tar.gz" Dockerfile.testbin chart -C cmd $BINARY_NAME
+echo "Created k8s-test-runner-$OS-v$VERSION.tar.gz"
