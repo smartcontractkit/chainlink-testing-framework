ff4f65442adfc183c2154bd2c962609649c4ac55
AnieeG <anindita.ghosh@smartcontract.com>
2024-03-27 16:01:23 -0700

    update path

--- k8s/environment/runner.go
+++ k8s/environment/runner.go
@@ -188,7 +188,7 @@ func container(props *Props) *[]*k8s.Container {
 				},
 				{
 					Name:      ptr.Ptr("reports"),
-					MountPath: ptr.Ptr("/integration-tests"),
+					MountPath: ptr.Ptr("/go/testdir/integration-tests"),
 				},
 			},
 		},
