85d230551f167887702c7e2fc45c21558c09074f
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-04-25 12:54:55 +0200

    add an universal concurrent executor that divides tasks equally between all workers

---
+++ concurrency/example.go
@@ -0,0 +1,94 @@
+package concurrency
+
+import (
+	"fmt"
+
+	"github.com/smartcontractkit/chainlink-testing-framework/logging"
+)
+
+type client struct{}
+
+func (c *client) getConcurrency() int {
+	return 1
+}
+
+func (c *client) deplyContractConfigurableFromKey(_ int, _ contractConfiguration) (ContractIntstance, error) {
+	return ContractIntstance{}, nil
+}
+
+func (c *client) deplyContractFromKey(_ int) (ContractIntstance, error) {
+	return ContractIntstance{}, nil
+}
+
+type ContractIntstance struct{}
+
+type contractConfiguration struct{}
+
+type contractResult struct {
+	instance ContractIntstance
+}
+
+func (k contractResult) GetResult() ContractIntstance {
+	return k.instance
+}
+
+// DeployContractsWithConfiguration shows a very simplified method that deploys concurrently contract instances with given configurations
+func DeployContractsWithConfiguration(client *client, contractConfigs []contractConfiguration) ([]ContractIntstance, error) {
+	l := logging.GetTestLogger(nil)
+
+	executor := NewConcurrentExecutor[ContractIntstance, contractResult, contractConfiguration](l)
+
+	var deployContractFn = func(channel chan contractResult, errorCh chan error, executorNum int, payload contractConfiguration) {
+		keyNum := executorNum + 1 // key 0 is the root key
+
+		instance, err := client.deplyContractConfigurableFromKey(keyNum, payload)
+		if err != nil {
+			errorCh <- err
+			return
+		}
+
+		channel <- contractResult{instance: instance}
+	}
+
+	results, err := executor.Execute(client.getConcurrency(), contractConfigs, deployContractFn)
+	if err != nil {
+		return []ContractIntstance{}, err
+	}
+
+	if len(results) != len(contractConfigs) {
+		return []ContractIntstance{}, fmt.Errorf("expected %v results, got %v", len(contractConfigs), len(results))
+	}
+
+	return results, nil
+}
+
+// DeployIdenticalContracts shows a very simplified method that deploys concurrently identical contract instances
+// which require no configuration, just need to be exected N amount of times
+func DeployIdenticalContracts(client *client, numberOfContracts int) ([]ContractIntstance, error) {
+	l := logging.GetTestLogger(nil)
+
+	executor := NewConcurrentExecutor[ContractIntstance, contractResult, NoTaskType](l)
+
+	var deployContractFn = func(channel chan contractResult, errorCh chan error, executorNum int) {
+		keyNum := executorNum + 1 // key 0 is the root key
+
+		instance, err := client.deplyContractFromKey(keyNum)
+		if err != nil {
+			errorCh <- err
+			return
+		}
+
+		channel <- contractResult{instance: instance}
+	}
+
+	results, err := executor.ExecuteSimple(client.getConcurrency(), numberOfContracts, deployContractFn)
+	if err != nil {
+		return []ContractIntstance{}, err
+	}
+
+	if len(results) != numberOfContracts {
+		return []ContractIntstance{}, fmt.Errorf("expected %v results, got %v", numberOfContracts, len(results))
+	}
+
+	return results, nil
+}
---
+++ concurrency/executor_test.go
@@ -0,0 +1,261 @@
+package concurrency_test
+
+import (
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/chainlink-testing-framework/concurrency"
+	"github.com/smartcontractkit/chainlink-testing-framework/logging"
+)
+
+type result struct {
+	integer int
+}
+
+func (r result) GetResult() int {
+	return r.integer
+}
+
+type config struct{}
+
+func TestExecute(t *testing.T) {
+
+	type tc struct {
+		name            string
+		concurrency     int
+		tasks           int
+		expectedResults int
+		expectedErrors  int
+		errorFn         func(*big.Int) error
+	}
+
+	tcs := []tc{
+		{
+			name:            "concurrency 1, tasks 1",
+			concurrency:     1,
+			tasks:           1,
+			expectedResults: 1,
+			expectedErrors:  0,
+		},
+		{
+			name:            "concurrency 10, tasks 1",
+			concurrency:     10,
+			tasks:           1,
+			expectedResults: 1,
+			expectedErrors:  0,
+		},
+		{
+			name:            "concurrency 2, tasks 5",
+			concurrency:     10,
+			tasks:           1,
+			expectedResults: 1,
+			expectedErrors:  0,
+		},
+		{
+			name:            "concurrency 0, tasks 1",
+			concurrency:     0,
+			tasks:           1,
+			expectedResults: 1,
+			expectedErrors:  0,
+		},
+		{
+			name:            "concurrency -1, tasks 1",
+			concurrency:     -1,
+			tasks:           1,
+			expectedResults: 1,
+			expectedErrors:  0,
+		},
+		{
+			name:            "concurrency 1, tasks 0",
+			concurrency:     1,
+			tasks:           0,
+			expectedResults: 0,
+			expectedErrors:  0,
+		},
+		{
+			name:            "concurrency 1, tasks 100, sporadic errors",
+			concurrency:     1,
+			tasks:           100,
+			expectedResults: 0,
+			expectedErrors:  1,
+			errorFn: func(integer *big.Int) error {
+				if integer.Int64()%2 == 0 {
+					return fmt.Errorf("even number error")
+				}
+				return nil
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+
+			l := logging.GetTestLogger(t)
+
+			executor := concurrency.NewConcurrentExecutor[int, result, config](l)
+
+			processorFn := func(resultCh chan result, errCh chan error, keyNum int, payload config) {
+				randomInt, err := rand.Int(rand.Reader, big.NewInt(100))
+				if err != nil {
+					errCh <- err
+					return
+				}
+
+				if tc.errorFn != nil {
+					err := tc.errorFn(randomInt)
+					if err != nil {
+						errCh <- err
+						return
+					}
+				}
+
+				resultCh <- result{integer: int(randomInt.Int64())}
+			}
+
+			configs := []config{}
+			for i := 0; i < tc.tasks; i++ {
+				configs = append(configs, struct{}{})
+			}
+
+			results, err := executor.Execute(tc.expectedResults, configs, processorFn)
+			if tc.expectedErrors == 0 {
+				require.NoError(t, err, "Error executing concurrently")
+				require.Len(t, results, tc.expectedResults, "Wrong result number")
+			} else {
+				require.Error(t, err, "No error returned when executing concurrently")
+				require.Len(t, results, 0, "Expected no results")
+				require.GreaterOrEqual(t, len(executor.GetErrors()), tc.expectedErrors, "Wrong error number")
+			}
+		})
+	}
+}
+
+func TestExecuteFailFast(t *testing.T) {
+
+	type tc struct {
+		name     string
+		executor *concurrency.ConcurrentExecutor[int, result, config]
+		failFast bool
+	}
+
+	tcs := []tc{
+		{
+			name:     "fail fast enabled",
+			executor: concurrency.NewConcurrentExecutor[int, result, config](logging.GetTestLogger(t)),
+			failFast: true,
+		},
+		{
+			name:     "fail fast disabled",
+			executor: concurrency.NewConcurrentExecutor(logging.GetTestLogger(t), concurrency.WithoutFailFast[int, result, config]()),
+			failFast: false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+
+			processorFn := func(resultCh chan result, errCh chan error, keyNum int, payload config) {
+				errCh <- errors.New("always fail")
+			}
+
+			expectedExecutions := 100
+
+			configs := []config{}
+			for i := 0; i < expectedExecutions; i++ {
+				configs = append(configs, struct{}{})
+			}
+
+			results, err := tc.executor.Execute(expectedExecutions, configs, processorFn)
+			require.Error(t, err, "No error returned when executing concurrently")
+			require.Len(t, results, 0, "Expected no results")
+			if tc.failFast {
+				require.Less(t, len(tc.executor.GetErrors()), expectedExecutions, "With fail fast enabled not all tasks should be executed")
+			} else {
+				require.Equal(t, len(tc.executor.GetErrors()), expectedExecutions, "With fail fast disabled all tasks should be executed")
+			}
+		})
+	}
+}
+
+func TestExecuteSimple(t *testing.T) {
+
+	type tc struct {
+		name            string
+		concurrency     int
+		tasks           int
+		expectedResults int
+		expectedErrors  int
+		errorFn         func(*big.Int) error
+	}
+
+	tcs := []tc{
+		{
+			name:            "concurrency 2, tasks 5",
+			concurrency:     10,
+			tasks:           1,
+			expectedResults: 1,
+			expectedErrors:  0,
+		},
+		{
+			name:            "concurrency 1, tasks 100, sporadic errors",
+			concurrency:     1,
+			tasks:           100,
+			expectedResults: 0,
+			expectedErrors:  1,
+			errorFn: func(integer *big.Int) error {
+				if integer.Int64()%2 == 0 {
+					return fmt.Errorf("even number error")
+				}
+				return nil
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+
+			l := logging.GetTestLogger(t)
+
+			executor := concurrency.NewConcurrentExecutor[int, result, concurrency.NoTaskType](l)
+
+			processorFn := func(resultCh chan result, errCh chan error, keyNum int) {
+				randomInt, err := rand.Int(rand.Reader, big.NewInt(100))
+				if err != nil {
+					errCh <- err
+					return
+				}
+
+				if tc.errorFn != nil {
+					err := tc.errorFn(randomInt)
+					if err != nil {
+						errCh <- err
+						return
+					}
+				}
+
+				resultCh <- result{integer: int(randomInt.Int64())}
+			}
+
+			results, err := executor.ExecuteSimple(tc.concurrency, tc.tasks, processorFn)
+			if tc.expectedErrors == 0 {
+				require.NoError(t, err, "Error executing concurrently")
+				require.Len(t, results, tc.expectedResults, "Wrong result number")
+			} else {
+				require.Error(t, err, "No error returned when executing concurrently")
+				require.Len(t, results, 0, "Expected no results")
+				require.GreaterOrEqual(t, len(executor.GetErrors()), tc.expectedErrors, "Wrong error number")
+			}
+		})
+	}
+}
