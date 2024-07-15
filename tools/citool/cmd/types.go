5595b2d234343f028e048e07131e98effab3fa41
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2024-07-15 13:38:52 +0200

    Add fields for required test config override and secrets

--- tools/citool/cmd/types.go
+++ tools/citool/cmd/types.go
@@ -7,17 +7,19 @@ type Test struct {
 
 // CITestConf defines the configuration for running a test in a CI environment, specifying details like test ID, path, type, runner settings, command, and associated workflows.
 type CITestConf struct {
-	ID                    string            `yaml:"id" json:"id"`
-	IDSanitized           string            `json:"idSanitized"`
-	Path                  string            `yaml:"path" json:"path"`
-	TestType              string            `yaml:"test-type" json:"testType"`
-	RunsOn                string            `yaml:"runs-on" json:"runsOn"`
-	TestCmd               string            `yaml:"test-cmd" json:"testCmd"`
-	DefaultTestInputs     map[string]string `yaml:"default-test-inputs" json:"defaultTestInputs"`
-	RemoteRunnerTestSuite string            `yaml:"remote-runner-test-suite" json:"remoteRunnerTestSuite"`
-	RemoteRunnerMemory    string            `yaml:"remote-runner-memory" json:"remoteRunnerMemory"`
-	PyroscopeEnv          string            `yaml:"pyroscope-env" json:"pyroscopeEnv"`
-	Workflows             []string          `yaml:"workflows" json:"workflows"`
+	ID                         string            `yaml:"id" json:"id"`
+	IDSanitized                string            `json:"idSanitized"`
+	Path                       string            `yaml:"path" json:"path"`
+	TestType                   string            `yaml:"test-type" json:"testType"`
+	RunsOn                     string            `yaml:"runs-on" json:"runsOn"`
+	TestCmd                    string            `yaml:"test-cmd" json:"testCmd"`
+	TestConfigOverrideRequired bool              `yaml:"test-config-override-required" json:"testConfigOverrideRequired"`
+	TestSecretsRequired        bool              `yaml:"test-secrets-required" json:"testSecretsRequired"`
+	DefaultTestInputs          map[string]string `yaml:"default-test-inputs" json:"defaultTestInputs"`
+	RemoteRunnerTestSuite      string            `yaml:"remote-runner-test-suite" json:"remoteRunnerTestSuite"`
+	RemoteRunnerMemory         string            `yaml:"remote-runner-memory" json:"remoteRunnerMemory"`
+	PyroscopeEnv               string            `yaml:"pyroscope-env" json:"pyroscopeEnv"`
+	Workflows                  []string          `yaml:"workflows" json:"workflows"`
 }
 
 type Config struct {
