8903d5044f27805f7dbfd297ad178435ab705825
skudasov <f4hrenh9it@gmail.com>
2024-09-16 10:51:13 +0200

    breaking change in k8s-test-runner

--- k8s-test-runner/cmd/internal/run.go
+++ k8s-test-runner/cmd/internal/run.go
@@ -69,7 +69,7 @@ func runRunE(cmd *cobra.Command, args []string) error {
 		return errors.Wrapf(err, "error creating test in k8s")
 	}
 
-	err = p.Run()
+	err = p.Run(0)
 	if err != nil {
 		return errors.Wrapf(err, "error running test in k8s")
 	}
