569a2f7979c8ccb985ccda3f6048384263070b1a
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-10-25 13:40:33 +0200

    do not run besu as root

--- lib/docker/test_env/besu_eth2.go
+++ lib/docker/test_env/besu_eth2.go
@@ -130,10 +130,10 @@ func (g *Besu) getEth2ContainerRequest() (*tc.ContainerRequest, error) {
 			tcwait.ForLog("Ethereum main loop is up").
 				WithPollInterval(1 * time.Second)).
 			WithStartupTimeoutDefault(g.StartupTimeout),
-		User: "0:0", //otherwise in CI we get "permission denied" error, when trying to access data from mounted volume
+		//User: "0:0", //otherwise in CI we get "permission denied" error, when trying to access data from mounted volume
 		Entrypoint: []string{
 			"sh",
-			"/root/init.sh",
+			"/init.sh",
 		},
 		//Cmd:  cmd,
 		Env: map[string]string{
@@ -142,7 +142,7 @@ func (g *Besu) getEth2ContainerRequest() (*tc.ContainerRequest, error) {
 		Files: []tc.ContainerFile{
 			{
 				HostFilePath:      initFile.Name(),
-				ContainerFilePath: "/root/init.sh",
+				ContainerFilePath: "/init.sh",
 				FileMode:          0744,
 			},
 		},
@@ -168,7 +168,7 @@ func (g *Besu) buildPosInitScript(params string) (string, error) {
 	echo "Copied genesis file to {{.ExecutionDir}}"
 	mkdir -p {{.ExecutionDir}}
 	cp {{.GeneratedDataDir}}/besu.json {{.ExecutionDir}}/besu.json
-	chmod 777 {{.GeneratedDataDir}}/genesis.json
+	# chmod 777 {{.GeneratedDataDir}}/genesis.json
 
 	echo "Starting Besu..."
 	echo "Running command: {{.Command}}"
