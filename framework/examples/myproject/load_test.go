d7c6f8a880492d32344a6f3cf31019d63084ee49
skudasov <f4hrenh9it@gmail.com>
2024-11-05 19:25:20 +0100

    set public ECR role workflow

--- framework/examples/myproject/load_test.go
+++ framework/examples/myproject/load_test.go
@@ -1,6 +1,7 @@
 package examples
 
 import (
+	"fmt"
 	"github.com/smartcontractkit/chainlink-testing-framework/framework"
 	"github.com/smartcontractkit/chainlink-testing-framework/framework/components/blockchain"
 	"github.com/smartcontractkit/chainlink-testing-framework/framework/components/fake"
@@ -12,14 +13,16 @@ import (
 )
 
 type CfgLoad struct {
-	BlockchainA        *blockchain.Input `toml:"blockchain_a" validate:"required"`
-	MockerDataProvider *fake.Input       `toml:"data_provider" validate:"required"`
-	NodeSet            *ns.Input         `toml:"nodeset" validate:"required"`
+	BlockchainA *blockchain.Input `toml:"blockchain_a" validate:"required"`
+	//Contracts          *onchain.Input    `toml:"contracts" validate:"required"`
+	MockerDataProvider *fake.Input `toml:"data_provider" validate:"required"`
+	NodeSet            *ns.Input   `toml:"nodeset" validate:"required"`
 }
 
 func TestLoad(t *testing.T) {
 	in, err := framework.Load[CfgLoad](t)
 	require.NoError(t, err)
+	//pkey := os.Getenv("PRIVATE_KEY")
 
 	bc, err := blockchain.NewBlockchainNetwork(in.BlockchainA)
 	require.NoError(t, err)
@@ -28,9 +31,20 @@ func TestLoad(t *testing.T) {
 	_, err = ns.NewSharedDBNodeSet(in.NodeSet, bc, dp.BaseURLDocker)
 	require.NoError(t, err)
 
+	//// deploy product contracts
+	//in.Contracts.URL = bc.Nodes[0].HostWSUrl
+	//contracts, err := onchain.NewProductOnChainDeployment(in.Contracts)
+	//require.NoError(t, err)
+	//
+	//sc, err := seth.NewClientBuilder().
+	//	WithRpcUrl(bc.Nodes[0].HostWSUrl).
+	//	WithGasPriceEstimations(true, 0, seth.Priority_Fast).
+	//	WithTracing(seth.TracingLevel_All, []string{seth.TraceOutput_Console}).
+	//	WithPrivateKeys([]string{pkey}).
+	//	Build()
+	//require.NoError(t, err)
+
 	t.Run("test something", func(t *testing.T) {
-		srv := wasp.NewHTTPMockServer(nil)
-		srv.Run()
 		labels := map[string]string{
 			"go_test_name": "generator_healthcheck",
 			"gen_name":     "generator_healthcheck",
@@ -38,9 +52,13 @@ func TestLoad(t *testing.T) {
 			"commit":       "generator_healthcheck",
 		}
 		gen, err := wasp.NewGenerator(&wasp.Config{
-			LoadType:   wasp.RPS,
-			Schedule:   wasp.Plain(5, 60*time.Second),
-			Gun:        NewExampleHTTPGun(srv.URL()),
+			LoadType: wasp.RPS,
+			Schedule: wasp.Combine(
+				wasp.Steps(1, 1, 9, 30*time.Second),
+				wasp.Plain(10, 30*time.Second),
+				wasp.Steps(10, -1, 10, 30*time.Second),
+			),
+			Gun:        NewExampleHTTPGun(fmt.Sprintf("%s/mock1", dp.BaseURLHost)),
 			Labels:     labels,
 			LokiConfig: wasp.NewEnvLokiConfig(),
 		})
