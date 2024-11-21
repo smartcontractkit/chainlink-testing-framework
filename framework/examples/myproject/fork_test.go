ff2262aef7b136d0a74ca8e7c425926ca289d3aa
skudasov <f4hrenh9it@gmail.com>
2024-11-21 10:49:33 +0100

    no mining configuration

--- framework/examples/myproject/fork_test.go
+++ framework/examples/myproject/fork_test.go
@@ -3,13 +3,16 @@ package examples
 import (
 	"github.com/smartcontractkit/chainlink-testing-framework/framework"
 	"github.com/smartcontractkit/chainlink-testing-framework/framework/components/blockchain"
+	ns "github.com/smartcontractkit/chainlink-testing-framework/framework/components/simple_node_set"
 	"github.com/smartcontractkit/chainlink-testing-framework/framework/rpc"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 type CfgFork struct {
 	BlockchainA *blockchain.Input `toml:"blockchain_a" validate:"required"`
+	NodeSet     *ns.Input         `toml:"nodeset" validate:"required"`
 }
 
 func TestFork(t *testing.T) {
@@ -19,9 +22,15 @@ func TestFork(t *testing.T) {
 	bc, err := blockchain.NewBlockchainNetwork(in.BlockchainA)
 	require.NoError(t, err)
 
+	_, err = ns.NewSharedDBNodeSet(in.NodeSet, bc)
+	require.NoError(t, err)
+
 	t.Run("test some contracts with fork", func(t *testing.T) {
-		ac := rpc.New(bc.Nodes[0].HostHTTPUrl, nil)
-		err = ac.AnvilAutoImpersonate(true)
-		require.NoError(t, err)
+		// deploy your contracts with 0s blocks
+		_ = rpc.New(bc.Nodes[0].HostHTTPUrl, nil)
+
+		// start a miner so CL nodes can update blocks
+		miner := rpc.NewRemoteAnvilMiner(bc.Nodes[0].HostHTTPUrl, nil)
+		miner.MinePeriodically(1 * time.Second)
 	})
 }
