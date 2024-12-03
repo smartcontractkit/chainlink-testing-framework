d8bab5b837fd124a807114d3ae9c478f868ae29c
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2024-12-03 15:41:56 +0100

    debug 2

--- tools/flakeguard/reports/path_mapper.go
+++ tools/flakeguard/reports/path_mapper.go
@@ -9,36 +9,41 @@ import (
 // MapTestResultsToPaths maps test results to their corresponding file paths.
 func MapTestResultsToPaths(report *TestReport, rootDir string) error {
 	// Scan the codebase for test functions
+	rootDir, err := filepath.Abs(rootDir)
+	if err != nil {
+		return fmt.Errorf("error normalizing rootDir: %v", err)
+	}
+
 	testFileMap, err := ScanTestFiles(rootDir)
 	if err != nil {
 		return err
 	}
+
+	fmt.Printf("Root Directory: %s\n", rootDir)
+	fmt.Printf("Test File Map: %+v\n", testFileMap)
 
 	// Assign file paths to each test result
 	for i, result := range report.Results {
 		testName := result.TestName
 		var filePath string
 
-		// Handle subtests
 		if strings.Contains(testName, "/") {
-			parentTestName := strings.SplitN(testName, "/", 2)[0] // Extract parent test
+			parentTestName := strings.SplitN(testName, "/", 2)[0]
 			if path, exists := testFileMap[parentTestName]; exists {
 				filePath = path
 			}
 		} else if path, exists := testFileMap[testName]; exists {
-			// Handle normal tests
 			filePath = path
 		}
 
-		// Normalize filePath to be relative to the project root
 		if filePath != "" {
 			relFilePath, err := filepath.Rel(rootDir, filePath)
 			if err != nil {
 				return fmt.Errorf("error getting relative path: %v", err)
 			}
-			report.Results[i].TestPath = relFilePath
+			report.Results[i].TestPath = filepath.ToSlash(relFilePath)
 		} else {
-			// Log or mark tests not found in the codebase
+			fmt.Printf("TestName not mapped: %s\n", testName)
 			report.Results[i].TestPath = "NOT FOUND"
 		}
 	}
