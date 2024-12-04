8f0ca5b5ace0a32be52573bef6f62df31c45f75c
skudasov <f4hrenh9it@gmail.com>
2024-12-04 16:43:06 +0100

    run JD component test

--- framework/components/jd/jd_test.go
+++ framework/components/jd/jd_test.go
@@ -14,7 +14,7 @@ import (
 // here we only test that we can boot up JD
 // client examples are under "examples" dir
 // since JD is private this env var should be set locally and in CI
-func TestJD(t *testing.T) {
+func TestComponentDockerJD(t *testing.T) {
 	err := framework.DefaultNetwork(&sync.Once{})
 	require.NoError(t, err)
 	pgOut, err := postgres.NewPostgreSQL(&postgres.Input{
