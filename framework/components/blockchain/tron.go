5b205d1751d4a92071d2a952fce705cdfd872245
skudasov <f4hrenh9it@gmail.com>
2025-01-17 11:54:54 +0100

    compiler version as param

--- framework/components/blockchain/tron.go
+++ framework/components/blockchain/tron.go
@@ -21,26 +21,7 @@ type Accounts struct {
 }
 
 const (
-	AccountsFile = `{
-		"hdPath": "m/44'/195'/0'/0/",
-		"mnemonic": "resemble birth wool happy sun burger fatal trumpet globe purity health ritual",
-		"privateKeys": [
-			"932a39242805a1b1095638027f26af9664d1d5bf8ab3b7527ee75e7efb2946dd",
-			"1c17c9c049d36cde7e5ea99df6c86e0474b04f0e258ab619a1e674f397a17152",
-			"458130a239671674746582184711a6f8d633355df1a491b9f3b323576134c2e9",
-			"2676fd1427968e07feaa9aff967d4ba7607c5497c499968c098d0517cd75cfbb",
-			"d26b24a691ff2b03ee6ab65bf164def216f73574996b9ca6299c43a9a63767ac",
-			"55df6adf3d081944dbe4688205d94f236fb4427ac44f3a286a96d47db0860667",
-			"8a9a60ddd722a40753c2a38edd6b6fa38e806d681c9b08a520ba4912e62b6458",
-			"75eb182fb623acf5e53d9885c4e8578f2530533a96c753481cc4277ecc6022de",
-			"6c4b22b1d9d68ef7a8ecd151cd4ffdd4ecc2a7b3a3f8a9f9f9bbdbcef6671f10",
-			"e578d66453cb41b6c923b9caa91c375a0545eeb171ccafc60b46fa834ce5c200"
-		],
-	 "more": []
-	}
-	`
-	DefaultTronPort         = "9090"
-	DefaultTronSolidityPort = "8091"
+	DefaultTronPort = "9090"
 )
 
 func defaultTron(in *Input) {
