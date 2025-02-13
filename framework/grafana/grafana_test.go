9b57389ac00c289d37892b50d4eea7828e36da14
Sergey Kudasov <f4hrenh9it@gmail.com>
2025-02-13 18:19:14 +0100

    multi-dashboard annotations, add RMN dummy (#1635)

--- framework/grafana/grafana_test.go
+++ framework/grafana/grafana_test.go
@@ -1,14 +1,13 @@
 package framework
 
 import (
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
-// TestPostAnnotationIntegration tests the PostAnnotation method against a real Grafana instance.
+// TestPostAnnotationIntegration tests the Annotation method against a real Grafana instance.
 func TestPostAnnotationIntegration(t *testing.T) {
 	t.Skip("manual grafana integration test")
 	grafanaURL := os.Getenv("GRAFANA_URL")
@@ -18,18 +17,21 @@ func TestPostAnnotationIntegration(t *testing.T) {
 	}
 	client := NewGrafanaClient(grafanaURL, apiKey)
 
-	annotation := PostAnnotation{
-		DashboardUID: "WaspDebug",
-		Text:         "CTFv2 test annotation",
-		Tags:         []string{"tag-1", "tag-2"},
-		Time:         Ptr(time.Now().Add(-1 * time.Minute)),
-		TimeEnd:      Ptr(time.Now()),
+	annotation := Annotation{
+		Text:      "CTFv2 test annotation",
+		StartTime: Ptr(time.Now().Add(-11 * time.Minute)),
+		//EndTime:      Ptr(time.Now()),
+		DashboardUID: []string{"WaspDebug", "e98b5451-12dc-4a8b-9576-2c0b67ddbd0c"},
+		Tags:         []string{"tag-3", "tag-4"},
 	}
-	response, resp, err := client.PostAnnotation(annotation)
-	assert.NoError(t, err, "PostAnnotation should not return an error")
-	assert.Equal(t, 200, resp.StatusCode(), "Expected HTTP status code 200")
-	assert.NotEmpty(t, response.ID, "Annotation ID should not be empty")
-	assert.NotEmpty(t, response.Message, "Annotation message should not be empty")
+	response, resp, err := client.Annotate(annotation)
+	assert.NoError(t, err, "Annotate should not return an error")
+	assert.Equal(t, 200, resp[0].StatusCode(), "Expected HTTP status code 200")
+	assert.NotEmpty(t, response[0].ID, "Annotation ID should not be empty")
+	assert.NotEmpty(t, response[0].Message, "Annotation message should not be empty")
+	assert.Equal(t, 200, resp[1].StatusCode(), "Expected HTTP status code 200")
+	assert.NotEmpty(t, response[1].ID, "Annotation ID should not be empty")
+	assert.NotEmpty(t, response[1].Message, "Annotation message should not be empty")
 }
 
 func Ptr[T any](value T) *T {
