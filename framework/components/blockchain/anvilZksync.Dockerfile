bcd756f4e0b8c4bf92b96f1a6e210c07bdf6a4dd
Ilan <ilan.olkies@smartcontract.com>
2025-02-21 12:10:48 -0300

    Enable chain id and port params + enable calls from any directory

--- framework/components/blockchain/anvilZksync.Dockerfile
+++
@@ -1,7 +0,0 @@
-# Use latest Ubuntu as the base image
-FROM ubuntu:latest
-
-RUN apt update
-RUN apt install -y curl git
-
-RUN curl -L https://raw.githubusercontent.com/matter-labs/foundry-zksync/main/install-foundry-zksync | bash
--- framework/components/blockchain/anvilZksync.go
+++ framework/components/blockchain/anvilZksync.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/testcontainers/testcontainers-go"
@@ -17,21 +19,51 @@ func defaultAnvilZksync(in *Input) {
 	}
 }
 
+const dockerFile = `FROM ubuntu:latest
+RUN apt update
+RUN apt install -y curl git
+RUN curl -L https://raw.githubusercontent.com/matter-labs/foundry-zksync/main/install-foundry-zksync | bash`
+
+// anvil-zskync has no public image builds. this method will build the image from source
+// creating a dockerfile in a temporary directory with the necessary commands to install
+// foundry-zksync.
+// see: https://foundry-book.zksync.io/getting-started/installation#using-foundry-with-docker
 func newAnvilZksync(in *Input) (*Output, error) {
 	defaultAnvilZksync(in)
 	req := baseRequest(in)
 
-	// anvil-zskync has no public image builds.
-	// see: https://foundry-book.zksync.io/getting-started/installation#using-foundry-with-docker
+	tempDir, err := os.MkdirTemp(".", "anvil-zksync-dockercontext-")
+	if err != nil {
+		return nil, err
+	}
+
+	dockerfilePath := filepath.Join(tempDir, "anvilZksync.Dockerfile")
+
+	if err := os.WriteFile(dockerfilePath, []byte(dockerFile), 0644); err != nil {
+		return nil, err
+	}
 	req.FromDockerfile = testcontainers.FromDockerfile{
-		Context:    ".",
+		Context:    tempDir,
 		Dockerfile: "anvilZksync.Dockerfile",
 		KeepImage:  true,
 	}
 
-	req.Entrypoint = []string{"/bin/sh", "-c", "/root/.foundry/bin/anvil-zksync --offline run"}
+	req.Entrypoint = []string{
+		"/bin/sh",
+		"-c",
+		"/root/.foundry/bin/anvil-zksync --offline --chain-id " + in.ChainID + " --port " + in.Port + " run",
+	}
 
 	framework.L.Info().Any("Cmd", strings.Join(req.Entrypoint, " ")).Msg("Creating anvil with command")
 
-	return createGenericEvmContainer(in, req)
+	output, err := createGenericEvmContainer(in, req)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := os.RemoveAll(tempDir); err != nil {
+		return nil, err
+	}
+
+	return output, nil
 }
