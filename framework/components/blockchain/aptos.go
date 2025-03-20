0ae0bb2b45a59665e8dda303b0bc288e8b6d3246
Friedemann Fürst <friedemann.fuerst@smartcontract.com>
2025-03-20 18:53:25 +0100

    Update Aptos node version to 1.27.2

--- framework/components/blockchain/aptos.go
+++ framework/components/blockchain/aptos.go
@@ -19,7 +19,7 @@ var (
 
 func defaultAptos(in *Input) {
 	if in.Image == "" {
-		in.Image = "aptoslabs/tools:aptos-node-v1.27.1"
+		in.Image = "aptoslabs/tools:aptos-node-v1.27.2"
 	}
 	if in.Port != "" {
 		framework.L.Warn().Msg("'port' field is set but only default port can be used: 8080")
