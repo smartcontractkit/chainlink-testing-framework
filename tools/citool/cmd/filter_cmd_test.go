d12aaf98c18c7711bdb211e4d0bc0a5317c80e02
Erik Burton <erikburt@users.noreply.github.com>
2025-06-23 14:14:35 -0700

    feat: add id for self-hosted runner label (#1911)

--- tools/citool/cmd/filter_cmd_test.go
+++ tools/citool/cmd/filter_cmd_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -58,6 +59,43 @@ func TestFilterTestsIntegration(t *testing.T) {
 			filtered := filterTests(tests, c.inputWorkflow, c.inputTestType, c.inputIDs, false)
 			if len(filtered) != c.expectedLen {
 				t.Errorf("FilterTests(%s) returned %d tests, expected %d", c.description, len(filtered), c.expectedLen)
+			}
+		})
+	}
+}
+
+func TestRunsOnSelfHostedID(t *testing.T) {
+	tests := []CITestConf{
+		{ID: "foo", TestEnvType: "typeA", RunsOnSelfHosted: "runs-on/cpu=0/ram=0"},
+		{ID: "bar", TestEnvType: "typeA", RunsOnSelfHosted: "runs-on/cpu=1/ram=1"},
+		{ID: "baz", TestEnvType: "typeB", RunsOnSelfHosted: "runs-on=foo/cpu=2/ram=2"},
+		{ID: "qux", TestEnvType: "typeC"},
+	}
+
+	cases := []struct {
+		description        string
+		inputTestType      string
+		expectedSelfHosted bool
+	}{
+		{"Insert epoch time", "typeA", true},
+		{"Dont insert epoch time", "typeB", true},
+		{"No self-hosted label", "typeC", false},
+	}
+
+	for _, c := range cases {
+		t.Run(c.description, func(t *testing.T) {
+			filtered := filterTests(tests, "", c.inputTestType, "", false)
+			for _, test := range filtered {
+				t.Logf("Test ID: %s, RunsOnSelfHosted: %s", test.ID, test.RunsOnSelfHosted)
+				if c.expectedSelfHosted {
+					if !strings.HasPrefix(test.RunsOnSelfHosted, "runs-on=") {
+						t.Errorf("Expected RunsOnSelfHosted to start with 'runs-on='. Got: %s", test.RunsOnSelfHosted)
+					}
+				} else {
+					if test.RunsOnSelfHosted != "" {
+						t.Errorf("Expected RunsOnSelfHosted to be empty. Got: %s", test.RunsOnSelfHosted)
+					}
+				}
 			}
 		})
 	}
