01fe53e8c1fe6180a5903163f64abe65dacc636e
Bartek Tofel <bartek.tofel@smartcontract.com>
2025-06-27 18:54:06 +0200

    [DX-1118] fix beholder (#1940)

--- framework/examples/myproject/smoke_chip_ingress_test.go
+++ framework/examples/myproject/smoke_chip_ingress_test.go
@@ -19,7 +19,8 @@ type ChipConfig struct {
 
 // use config file: smoke_chip.toml
 func TestChipIngressSmoke(t *testing.T) {
-	t.Skip("skipping smoke test until we have a way to fetch Chip Ingress image")
+	// t.Skip("skipping smoke test until we have a way to fetch Chip Ingress image")
+	os.Setenv("CTF_CONFIGS", "smoke_chip.toml")
 	in, err := framework.Load[ChipConfig](t)
 	require.NoError(t, err, "failed to load config")
 
