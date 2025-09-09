f795c807878e0b03a85577c7faad6f379d3f464e
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2025-09-09 10:03:01 -0600

    update billing with env vars and input configs

--- framework/components/dockercompose/billing_platform_service/billing_platform_service.go
+++ framework/components/dockercompose/billing_platform_service/billing_platform_service.go
@@ -3,15 +3,24 @@ package billing_platform_service
 import (
 	"context"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 	"github.com/pkg/errors"
-	"github.com/smartcontractkit/chainlink-testing-framework/framework"
-	"github.com/smartcontractkit/chainlink-testing-framework/framework/components/dockercompose/utils"
+	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/compose"
 	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/smartcontractkit/chainlink-testing-framework/framework"
+	"github.com/smartcontractkit/chainlink-testing-framework/framework/components/dockercompose/utils"
+	"github.com/smartcontractkit/freeport"
 )
+
+const DefaultPostgresDSN = "postgres://postgres:postgres@postgres:5432/billing_platform?sslmode=disable"
 
 type Output struct {
 	BillingPlatformService *BillingPlatformServiceOutput
@@ -19,8 +28,10 @@ type Output struct {
 }
 
 type BillingPlatformServiceOutput struct {
-	GRPCInternalURL string
-	GRPCExternalURL string
+	BillingGRPCInternalURL string
+	BillingGRPCExternalURL string
+	CreditGRPCInternalURL  string
+	CreditGRPCExternalURL  string
 }
 
 type PostgresOutput struct {
@@ -28,10 +39,18 @@ type PostgresOutput struct {
 }
 
 type Input struct {
-	ComposeFile         string   `toml:"compose_file"`
-	ExtraDockerNetworks []string `toml:"extra_docker_networks"`
-	Output              *Output  `toml:"output"`
-	UseCache            bool     `toml:"use_cache"`
+	ComposeFile                 string   `toml:"compose_file"`
+	ExtraDockerNetworks         []string `toml:"extra_docker_networks"`
+	Output                      *Output  `toml:"output"`
+	UseCache                    bool     `toml:"use_cache"`
+	ChainSelector               uint64   `toml:"chain_selector"`
+	StreamsAPIURL               string   `toml:"streams_api_url"`
+	StreamsAPIKey               string   `toml:"streams_api_key"`
+	StreamsAPISecret            string   `toml:"streams_api_secret"`
+	RPCURL                      string   `toml:"rpc_url"`
+	WorkflowRegistryAddress     string   `toml:"workflow_registry_address"`
+	CapabilitiesRegistryAddress string   `toml:"capabilities_registry_address"`
+	WorkflowOwners              []string `toml:"workflow_owners"`
 }
 
 func defaultBillingPlatformService(in *Input) *Input {
@@ -44,8 +63,11 @@ func defaultBillingPlatformService(in *Input) *Input {
 const (
 	DEFAULT_STACK_NAME = "billing-platform-service"
 
-	DEFAULT_BILLING_PLATFORM_SERVICE_GRPC_PORT    = "2022"
-	DEFAULT_BILLING_PLATFORM_SERVICE_SERVICE_NAME = "billing-platform-service"
+	DEFAULT_BILLING_PLATFORM_SERVICE_BILLING_GRPC_PORT = "2222"
+	DEFAULT_BILLING_PLATFORM_SERVICE_CREDIT_GRPC_PORT  = "2223"
+	DEFAULT_POSTGRES_PORT                              = "5432"
+	DEFAULT_BILLING_PLATFORM_SERVICE_SERVICE_NAME      = "billing-platform-service"
+	DEFAULT_POSTGRES_SERVICE_NAME                      = "postgres"
 )
 
 func New(in *Input) (*Output, error) {
@@ -79,7 +101,48 @@ func New(in *Input) (*Output, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 	defer cancel()
 
-	upErr := stack.Up(ctx)
+	// Start the stackwith all environment variables from the host process
+	// set development defaults for necessary environment variables and allow them to be overridden by the host process
+	envVars := make(map[string]string)
+
+	envVars["MAINNET_WORKFLOW_REGISTRY_CHAIN_SELECTOR"] = strconv.FormatUint(in.ChainSelector, 10)
+	envVars["MAINNET_WORKFLOW_REGISTRY_CONTRACT_ADDRESS"] = in.WorkflowRegistryAddress
+	envVars["MAINNET_WORKFLOW_REGISTRY_RPC_URL"] = in.RPCURL
+	envVars["MAINNET_WORKFLOW_REGISTRY_FINALITY_DEPTH"] = "0"                    // Instant finality on devnet
+	envVars["KMS_PROOF_SIGNING_KEY_ID"] = "00000000-0000-0000-0000-000000000001" // provisioned via LocalStack
+	envVars["VERIFIER_INITIAL_INTERVAL"] = "0s"                                  // reduced to force verifier to start immediately in integration tests
+	envVars["VERIFIER_MAXIMUM_INTERVAL"] = "1s"                                  // reduced to force verifier to start immediately in integration tests
+	envVars["LINKING_REQUEST_COOLDOWN"] = "0s"                                   // reduced to force consequtive linking requests to be processed immediately in integration tests
+
+	envVars["MAINNET_CAPABILITIES_REGISTRY_CHAIN_SELECTOR"] = strconv.FormatUint(in.ChainSelector, 10)
+	envVars["MAINNET_CAPABILITIES_REGISTRY_CONTRACT_ADDRESS"] = in.CapabilitiesRegistryAddress
+	envVars["MAINNET_CAPABILITIES_REGISTRY_RPC_URL"] = in.RPCURL
+	envVars["MAINNET_CAPABILITIES_REGISTRY_FINALITY_DEPTH"] = "10" // Arbitrary value, adjust as needed
+
+	envVars["TEST_OWNERS"] = strings.Join(in.WorkflowOwners, ",")
+	envVars["STREAMS_API_URL"] = in.StreamsAPIURL
+	envVars["STREAMS_API_KEY"] = in.StreamsAPIKey
+	envVars["STREAMS_API_SECRET"] = in.StreamsAPISecret
+
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		if len(pair) == 2 {
+			envVars[pair[0]] = pair[1]
+		}
+	}
+
+	// set these env vars after reading env vars from host
+	port, err := freeport.Take(1)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get free port for Billing Platform Service postgres")
+	}
+
+	envVars["POSTGRES_PORT"] = strconv.FormatInt(int64(port[0]), 10)
+	envVars["DEFAULT_DSN"] = DefaultPostgresDSN
+
+	upErr := stack.
+		WithEnv(envVars).
+		Up(ctx)
 
 	if upErr != nil {
 		return nil, errors.Wrap(upErr, "failed to start stack for Billing Platform Service")
@@ -88,13 +151,19 @@ func New(in *Input) (*Output, error) {
 	stack.WaitForService(DEFAULT_BILLING_PLATFORM_SERVICE_SERVICE_NAME,
 		wait.ForAll(
 			wait.ForLog("GRPC server is live").WithPollInterval(200*time.Millisecond),
-			wait.ForListeningPort(DEFAULT_BILLING_PLATFORM_SERVICE_GRPC_PORT),
+			wait.ForListeningPort(DEFAULT_BILLING_PLATFORM_SERVICE_BILLING_GRPC_PORT),
+			wait.ForListeningPort(DEFAULT_BILLING_PLATFORM_SERVICE_CREDIT_GRPC_PORT),
 		).WithDeadline(1*time.Minute),
 	)
 
 	billingContainer, billingErr := stack.ServiceContainer(ctx, DEFAULT_BILLING_PLATFORM_SERVICE_SERVICE_NAME)
 	if billingErr != nil {
 		return nil, errors.Wrap(billingErr, "failed to get billing-platform-service container")
+	}
+
+	postgresContainer, postgresErr := stack.ServiceContainer(ctx, DEFAULT_POSTGRES_SERVICE_NAME)
+	if postgresErr != nil {
+		return nil, errors.Wrap(postgresErr, "failed to get postgres container")
 	}
 
 	cli, cliErr := client.NewClientWithOpts(
@@ -139,23 +208,61 @@ func New(in *Input) (*Output, error) {
 		return nil, errors.Wrap(billingExternalHostErr, "failed to get host for Billing Platform Service")
 	}
 
-	// get mapped port for billing platform service
-	billingExternalPort, billingExternalPortErr := utils.FindMappedPort(ctx, 20*time.Second, billingContainer, DEFAULT_BILLING_PLATFORM_SERVICE_GRPC_PORT)
-	if billingExternalPortErr != nil {
-		return nil, errors.Wrap(billingExternalPortErr, "failed to get mapped port for Chip Ingress")
+	// get hosts for billing platform service
+	postgresExternalHost, postgresExternalHostErr := utils.GetContainerHost(ctx, postgresContainer)
+	if postgresExternalHostErr != nil {
+		return nil, errors.Wrap(postgresExternalHostErr, "failed to get host for postgres")
+	}
+
+	// get mapped ports for billing platform service
+	serviceOutput, err := getExternalPorts(ctx, billingExternalHost, billingContainer)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get mapped port for Billing Platform Service")
+	}
+
+	externalPostgresPort, err := utils.FindMappedPort(ctx, 20*time.Second, postgresContainer, nat.Port(DEFAULT_POSTGRES_PORT+"/tcp"))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get mapped port for postgres")
 	}
 
 	output := &Output{
-		BillingPlatformService: &BillingPlatformServiceOutput{
-			GRPCInternalURL: fmt.Sprintf("http://%s:%s", DEFAULT_BILLING_PLATFORM_SERVICE_SERVICE_NAME, DEFAULT_BILLING_PLATFORM_SERVICE_GRPC_PORT),
-			GRPCExternalURL: fmt.Sprintf("http://%s:%s", billingExternalHost, billingExternalPort.Port()),
-		},
+		BillingPlatformService: serviceOutput,
 		Postgres: &PostgresOutput{
-
-		}
+			DSN: fmt.Sprintf("postgres://postgres:postgres@%s:%s/billing_platform", postgresExternalHost, externalPostgresPort.Port()),
+		},
 	}
 
 	framework.L.Info().Msg("Billing Platform Service stack start")
 
 	return output, nil
+}
+
+func getExternalPorts(ctx context.Context, billingExternalHost string, billingContainer *testcontainers.DockerContainer) (*BillingPlatformServiceOutput, error) {
+	ports := map[string]nat.Port{
+		"billing": DEFAULT_BILLING_PLATFORM_SERVICE_BILLING_GRPC_PORT,
+		"credit":  DEFAULT_BILLING_PLATFORM_SERVICE_CREDIT_GRPC_PORT,
+	}
+
+	output := BillingPlatformServiceOutput{}
+
+	for name, defaultPort := range ports {
+		externalPort, err := utils.FindMappedPort(ctx, 20*time.Second, billingContainer, defaultPort)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to get mapped port for Billing Platform Service")
+		}
+
+		internal := fmt.Sprintf("http://%s:%s", DEFAULT_BILLING_PLATFORM_SERVICE_SERVICE_NAME, defaultPort)
+		external := fmt.Sprintf("http://%s:%s", billingExternalHost, externalPort.Port())
+
+		switch name {
+		case "billing":
+			output.BillingGRPCInternalURL = internal
+			output.BillingGRPCExternalURL = external
+		case "credit":
+			output.CreditGRPCInternalURL = internal
+			output.CreditGRPCExternalURL = external
+		}
+	}
+
+	return &output, nil
 }
